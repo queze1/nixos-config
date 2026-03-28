3e0397a0fe390aaded65fbbfb045df4dfa3b35fc
queze1 <52340127+queze1@users.noreply.github.com>
2026-03-28 17:33:59 +1100

    feat: rename ctrl-d and ctrl-u to also centre screen in neovim

--- home/modules/programs/nixvim.nix
+++ home/modules/programs/nixvim.nix
@@ -2,4 +2,22 @@
 {
   colorschemes.catppuccin.enable = true;
   plugins.lualine.enable = true;
+
+  keymaps = [
+    # Scroll down half a page and center the cursor
+    {
+      mode = "n";
+      key = "<C-d>";
+      action = "<C-d>zz";
+      options.desc = "Scroll down half a page and center";
+    }
+
+    # Scroll up half a page and center the cursor
+    {
+      mode = "n";
+      key = "<C-u>";
+      action = "<C-u>zz";
+      options.desc = "Scroll up half a page and center";
+    }
+  ];
 }
