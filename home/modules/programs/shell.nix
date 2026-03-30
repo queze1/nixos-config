82ab26b95397aa5521348b71d2fb4c610af8ed84
queze1 <52340127+queze1@users.noreply.github.com>
2026-03-30 16:41:38 +1100

    fix: readd bash integration

--- home/modules/programs/shell.nix
+++ home/modules/programs/shell.nix
@@ -1,9 +1,11 @@
 { pkgs, ... }:
 {
   programs = {
+    bash.enable = true;
     fish.enable = true;
     direnv = {
       enable = true;
+      enableBashIntegration = true;
       enableFishIntegration = true;
       nix-direnv.enable = true;
     };
