f8400e21a285cd78b35afc1ab25a30eec4f2492f
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-02 17:38:09 +1100

    feat: make alacritty use fish

--- home/modules/programs/default.nix
+++ home/modules/programs/default.nix
@@ -42,6 +42,12 @@
   ];
 
   programs = {
+    alacritty = {
+      enable = true;
+      settings = {
+        terminal.shell.program = "${pkgs.fish}/bin/fish";
+      };
+    };
     firefox.enable = true;
     java = {
       enable = true;
