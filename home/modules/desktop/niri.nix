8df7fa51fe4437d879ce6c516600d182cf74369a
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-02 19:41:20 +1100

    feat: replace niri utils with noctalia

--- home/modules/desktop/niri.nix
+++ home/modules/desktop/niri.nix
@@ -5,16 +5,7 @@
 
 {
   xdg.configFile."niri/config.kdl".source = ./niri-config.kdl;
-
-  programs.fuzzel.enable = true; # Super+D in the default setting (app launcher)
-  programs.swaylock.enable = true; # Super+Alt+L in the default setting (screen locker)
-  programs.waybar.enable = true; # launch on startup in the default setting (bar)
-  services.mako.enable = true; # notification daemon
-  services.swayidle.enable = true; # idle management daemon
-  services.polkit-gnome.enable = true; # polkit
-
   home.packages = with pkgs; [
-    swaybg # wallpaper
     xwayland-satellite # xwayland support
   ];
 }
