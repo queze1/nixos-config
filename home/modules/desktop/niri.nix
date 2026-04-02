f122f0d226eaa5036ae886e17c3212170b2ed350
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-02 17:48:58 +1100

    fix: remove horizontal scrollkeybinds

--- home/modules/desktop/niri.nix
+++ home/modules/desktop/niri.nix
@@ -6,7 +6,6 @@
 {
   xdg.configFile."niri/config.kdl".source = ./niri-config.kdl;
 
-  programs.alacritty.enable = true; # Super+T in the default setting (terminal)
   programs.fuzzel.enable = true; # Super+D in the default setting (app launcher)
   programs.swaylock.enable = true; # Super+Alt+L in the default setting (screen locker)
   programs.waybar.enable = true; # launch on startup in the default setting (bar)
