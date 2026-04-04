28506faae37a93224d807b7c9ba3d18d1e1256f5
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-04 16:09:10 +1100

    feat: swap to kitty

--- home/modules/programs/default.nix
+++ home/modules/programs/default.nix
@@ -1,26 +1,14 @@
 {
   pkgs,
   pkgs-stable,
-  config,
-  hypervisor,
   ...
 }:
-let
-  # Duplicated from /home/queze/etc/nixos/home/modules/desktop/plasma-manager.nix
-  # Factor out later
-  homeDirectory =
-    if hypervisor == "vmware" then
-      "/mnt/hgfs/VMShared"
-    else if hypervisor == "utm" then
-      "/mnt/utm"
-    else
-      config.home.homeDirectory;
-in
+
 {
   imports = [
+    ./firefox.nix
     ./git.nix
     ./shell.nix
-    ./syncthing.nix
   ];
 
   home.packages = with pkgs; [
@@ -63,95 +51,6 @@ in
   ];
 
   programs = {
-    alacritty = {
-      enable = true;
-      settings = {
-        terminal.shell.program = "${pkgs.fish}/bin/fish";
-      };
-    };
-    firefox = {
-      enable = true;
-      policies = {
-        DisableTelemetry = true;
-        GenerativeAI = false;
-        OfferToSaveLoginsDefault = false;
-        DefaultDownloadDirectory = "${homeDirectory}/Downloads";
-      };
-
-      profiles.default = {
-        settings = {
-          "browser.tabs.insertAfterCurrent" = true;
-          "browser.aboutConfig.showWarning" = false;
-          "sidebar.verticalTabs" = true;
-          "widget.use-xdg-desktop-portal.file-picker" = 1;
-          "gfx.font_rendering.fontconfig.max_generic_substitution" = 127;
-        };
-
-        search = {
-          force = true;
-          default = "ddg";
-          privateDefault = "ddg";
-
-          engines = {
-            "Nix Packages" = {
-              urls = [
-                {
-                  template = "https://search.nixos.org/packages";
-                  params = [
-                    {
-                      name = "channel";
-                      value = "unstable";
-                    }
-                    {
-                      name = "query";
-                      value = "{searchTerms}";
-                    }
-                  ];
-                }
-              ];
-              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = [ "@np" ];
-            };
-
-            "Nix Options" = {
-              urls = [
-                {
-                  template = "https://search.nixos.org/options";
-                  params = [
-                    {
-                      name = "channel";
-                      value = "unstable";
-                    }
-                    {
-                      name = "query";
-                      value = "{searchTerms}";
-                    }
-                  ];
-                }
-              ];
-              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = [ "@no" ];
-            };
-
-            "NixOS Wiki" = {
-              urls = [
-                {
-                  template = "https://wiki.nixos.org/w/index.php";
-                  params = [
-                    {
-                      name = "search";
-                      value = "{searchTerms}";
-                    }
-                  ];
-                }
-              ];
-              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = [ "@nw" ];
-            };
-          };
-        };
-      };
-    };
     java = {
       enable = true;
       package = pkgs.jdk21;
@@ -161,6 +60,9 @@ in
       enable = true;
       package = pkgs.vscode.fhs;
     };
-    yazi.enable = true;
+  };
+
+  services.syncthing = {
+    enable = true;
   };
 }
