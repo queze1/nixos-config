42ebcad0b5b77976ca7d7527a7d29bba92384d8f
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-07 09:46:11 +1000

    fix: move thunar to system

--- system/common/programs.nix
+++ system/common/programs.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
 {
+  programs.fish.enable = true;
+  programs.nix-ld.enable = true;
+
   programs.thunar.enable = true;
   programs.thunar.plugins = with pkgs.xfce; [
     thunar-archive-plugin
