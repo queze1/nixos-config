9dc8a53223663bb52785f80742496af0be6d5a5a
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-07 18:58:25 +1000

    feat: add folders to syncthing

--- home/modules/programs/default.nix
+++ home/modules/programs/default.nix
@@ -65,5 +65,24 @@
 
   services.syncthing = {
     enable = true;
+    settings = {
+      devices = {
+        "poco-x3-pro" = {
+          id = "CGN4GSA-JX3232W-WM5XXI6-RKU3W6F-RVAZH7N-YPOCAF3-52SRDUO-HHRFFQI";
+        };
+      };
+      folders = {
+        "SillyTavern Data" = {
+          id = "nicrf-adfwa";
+          path = "/mnt/utm/Apps/SillyTavern-Launcher/SillyTavern/data/default-user";
+          devices = [ "poco-x3-pro" ];
+        };
+        "Music" = {
+          id = "ft74r-2c4sc";
+          path = "/mnt/utm/Music";
+          devices = [ "poco-x3-pro" ];
+        };
+      };
+    };
   };
 }
