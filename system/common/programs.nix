26d6ac9d6df0a15edc4637971cb8af985ae8ab37
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-10 15:53:31 +1000

    feat: remove thunar

--- system/common/programs.nix
+++ system/common/programs.nix
@@ -1,12 +1,5 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs.fish.enable = true;
   programs.nix-ld.enable = true;
-
-  programs.thunar.enable = true;
-  programs.thunar.plugins = with pkgs.xfce; [
-    thunar-archive-plugin
-    thunar-volman
-  ];
-  services.gvfs.enable = true;
 }
