497c9f158ca17f84b5c99d9c035915112ebc0b36
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-13 15:05:49 +1000

    fix: add neoscroll to nvf correctly

--- home/modules/programs/nvf.nix
+++ home/modules/programs/nvf.nix
@@ -67,9 +67,6 @@
         # Features & Plugins
         # ----------------------------------------
 
-        # Smooth scrolling
-        visuals.neoscroll.enable = true;
-
         # Multi-cursor
         utility.multicursors.enable = true;
 
@@ -85,11 +82,37 @@
         # Jumping (flash.nvim)
         utility.motion.flash-nvim.enable = true;
 
-        # Ability to move based on indentation (vim-indentwise)
         extraPlugins = with pkgs.vimPlugins; {
+          # Ability to move based on indentation (vim-indentwise)
           vim-indentwise = {
             package = vim-indentwise;
             setup = "";
+          };
+          # Smooth scrolling (neoscroll)
+          neoscroll = {
+            package = neoscroll-nvim;
+            setup = ''
+              require('neoscroll').setup({
+                mappings = {                 -- Keys to be mapped to their corresponding default scrolling animation
+                  '<C-u>', '<C-d>',
+                  '<C-b>', '<C-f>',
+                  '<C-y>', '<C-e>',
+                  'zt', 'zz', 'zb',
+                },
+                hide_cursor = true,          -- Hide cursor while scrolling
+                stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+                respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+                cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+                duration_multiplier = 1.0,   -- Global duration multiplier
+                easing = 'linear',           -- Default easing function
+                pre_hook = nil,              -- Function to run before the scrolling animation starts
+                post_hook = nil,             -- Function to run after the scrolling animation ends
+                performance_mode = false,    -- Disable "Performance Mode" on all buffers.
+                ignored_events = {           -- Events ignored while scrolling
+                    'WinScrolled', 'CursorMoved'
+                },
+              })
+            '';
           };
         };
 
