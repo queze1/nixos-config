44922d62e5a021fe668aafa9180faaabdc16965b
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-13 15:15:29 +1000

    feat: modify neoscroll animations

--- home/modules/programs/nvf.nix
+++ home/modules/programs/nvf.nix
@@ -92,19 +92,14 @@
           neoscroll = {
             package = neoscroll-nvim;
             setup = ''
-              require('neoscroll').setup({
-                mappings = {                 -- Keys to be mapped to their corresponding default scrolling animation
-                  '<C-u>', '<C-d>',
-                  '<C-b>', '<C-f>',
-                  '<C-y>', '<C-e>',
-                  'zt', 'zz', 'zb',
-                },
+              neoscroll = require('neoscroll')
+              neoscroll.setup({
                 hide_cursor = true,          -- Hide cursor while scrolling
                 stop_eof = true,             -- Stop at <EOF> when scrolling downwards
                 respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
                 cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
                 duration_multiplier = 1.0,   -- Global duration multiplier
-                easing = 'linear',           -- Default easing function
+                easing = 'quadratic',        -- Easing function (linear default)
                 pre_hook = nil,              -- Function to run before the scrolling animation starts
                 post_hook = nil,             -- Function to run after the scrolling animation ends
                 performance_mode = false,    -- Disable "Performance Mode" on all buffers.
@@ -112,6 +107,23 @@
                     'WinScrolled', 'CursorMoved'
                 },
               })
+
+              -- Set scrolling animations
+              local keymap = {
+                ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 250 }) end;
+                ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 250 }) end;
+                ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 450 }) end;
+                ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 450 }) end;
+                ["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor=false; duration = 100 }) end;
+                ["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor=false; duration = 100 }) end;
+                ["zt"]    = function() neoscroll.zt({ half_win_duration = 150 }) end; -- 250 default
+                ["zz"]    = function() neoscroll.zz({ half_win_duration = 150 }) end; -- 250 default
+                ["zb"]    = function() neoscroll.zb({ half_win_duration = 150 }) end; -- 250 default
+              }
+              local modes = { 'n', 'v', 'x' }
+              for key, func in pairs(keymap) do
+                vim.keymap.set(modes, key, func)
+              end
             '';
           };
         };
