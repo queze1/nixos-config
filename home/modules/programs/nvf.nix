89172012d3ccdc4c468d1b6ad31442261885c73d
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-13 15:28:18 +1000

    feat: replace neoscroll with cinnamon

--- home/modules/programs/nvf.nix
+++ home/modules/programs/nvf.nix
@@ -88,44 +88,34 @@
             package = vim-indentwise;
             setup = "";
           };
-          # Smooth scrolling (neoscroll)
-          neoscroll = {
-            package = neoscroll-nvim;
-            setup = ''
-              neoscroll = require('neoscroll')
-              neoscroll.setup({
-                hide_cursor = true,          -- Hide cursor while scrolling
-                stop_eof = true,             -- Stop at <EOF> when scrolling downwards
-                respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-                cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-                duration_multiplier = 1.0,   -- Global duration multiplier
-                easing = 'quadratic',        -- Easing function (linear default)
-                pre_hook = nil,              -- Function to run before the scrolling animation starts
-                post_hook = nil,             -- Function to run after the scrolling animation ends
-                performance_mode = false,    -- Disable "Performance Mode" on all buffers.
-                ignored_events = {           -- Events ignored while scrolling
-                    'WinScrolled', 'CursorMoved'
-                },
-              })
 
-              -- Set scrolling animations
-              local keymap = {
-                ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 250 }) end;
-                ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 250 }) end;
-                ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 450 }) end;
-                ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 450 }) end;
-                ["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor=false; duration = 100 }) end;
-                ["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor=false; duration = 100 }) end;
-                ["zt"]    = function() neoscroll.zt({ half_win_duration = 150 }) end; -- 250 default
-                ["zz"]    = function() neoscroll.zz({ half_win_duration = 150 }) end; -- 250 default
-                ["zb"]    = function() neoscroll.zb({ half_win_duration = 150 }) end; -- 250 default
-              }
-              local modes = { 'n', 'v', 'x' }
-              for key, func in pairs(keymap) do
-                vim.keymap.set(modes, key, func)
-              end
-            '';
-          };
+          # Smooth scrolling (neoscroll)
+          # neoscroll = {
+          #   package = neoscroll-nvim;
+          #   setup = ''
+          #     neoscroll = require('neoscroll')
+          #     neoscroll.setup({
+          #       easing = 'quadratic',
+          #     })
+          #
+          #     -- Set scrolling animations
+          #     local keymap = {
+          #       ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 250 }) end;
+          #       ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 250 }) end;
+          #       ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 450 }) end;
+          #       ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 450 }) end;
+          #       ["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor=false; duration = 100 }) end;
+          #       ["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor=false; duration = 100 }) end;
+          #       ["zt"]    = function() neoscroll.zt({ half_win_duration = 150 }) end; -- 250 default
+          #       ["zz"]    = function() neoscroll.zz({ half_win_duration = 150 }) end; -- 250 default
+          #       ["zb"]    = function() neoscroll.zb({ half_win_duration = 150 }) end; -- 250 default
+          #     }
+          #     local modes = { 'n', 'v', 'x' }
+          #     for key, func in pairs(keymap) do
+          #       vim.keymap.set(modes, key, func)
+          #     end
+          #   '';
+          # };
         };
 
         # A tree plugin (neo-tree)
@@ -157,6 +147,9 @@
             file4 = "<C-m>";
           };
         };
+
+        # Enable smooth scrolling
+        visuals.cinnamon-nvim.enable = true;
 
         # Copilot integration
         assistant.copilot = {
