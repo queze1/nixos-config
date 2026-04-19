13a183607befd709a0e41e8a945517dcb8880cfc
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-19 15:20:34 +1000

    refactor: move yazi to yazi.nix

--- home/modules/programs/default.nix
+++ home/modules/programs/default.nix
@@ -1,5 +1,4 @@
 {
-  inputs,
   pkgs,
   pkgs-stable,
   ...
@@ -11,6 +10,7 @@
     ./git.nix
     ./shell.nix
     ./nvf.nix
+    ./yazi.nix
   ];
 
   home.packages = with pkgs; [
@@ -64,41 +64,6 @@
     vscode = {
       enable = true;
       package = pkgs.vscode.fhs;
-    };
-    yazi = {
-      enable = true;
-      shellWrapperName = "y";
-      plugins.bunny = "${inputs.bunny-yazi}";
-      initLua = ''
-        require("bunny"):setup({
-          hops = {
-            { key = "/",          path = "/",                                      },
-            { key = "~",          path = "~",                                      },
-            { key = "t",          path = "/tmp",                                   },
-            { key = "n",          path = "/nix/store",         desc = "Nix store"  },
-            { key = "c",          path = "~/etc/nixos",        desc = "Nix config" },
-            { key = "C",          path = "~/Coding",           desc = "Coding"     },
-            { key = "m",          path = "/mnt/utm/Music",                         },
-            { key = "d",          path = "/mnt/utm/Downloads",                     },
-            { key = "D",          path = "/mnt/utm/Documents",                     },
-            { key = "p",          path = "/mnt/utm/Pictures",                      },
-            { key = "v",          path = "/mnt/utm/Videos",                        },
-            { key = "o",          path = "/mnt/utm/Documents/obsidian",            },
-          },
-          desc_strategy = "path", -- If desc isn't present, use "path" or "filename", default is "path"
-          ephemeral = true, -- Enable ephemeral hops, default is true
-          tabs = true, -- Enable tab hops, default is true
-          notify = false, -- Notify after hopping, default is false
-          fuzzy_cmd = "fzf", -- Fuzzy searching command, default is "fzf"
-        })
-      '';
-      keymap.mgr.prepend_keymap = [
-        {
-          on = ";";
-          run = "plugin bunny";
-          desc = "Start bunny.yazi";
-        }
-      ];
     };
   };
 
