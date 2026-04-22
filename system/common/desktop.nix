cf51b4c713b733ce3fde74039bfd9e4f09286286
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-22 10:01:59 +1000

    fix: disable ozone wayland

--- system/common/desktop.nix
+++ system/common/desktop.nix
@@ -9,9 +9,9 @@
     wayland.enable = true;
   };
 
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-  };
+  # environment.sessionVariables = {
+  #   NIXOS_OZONE_WL = "1";
+  # };
 
   xdg.portal = {
     extraPortals = with pkgs; [
