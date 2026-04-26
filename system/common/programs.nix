6938d897353b4abc5dd52906000efeae683fad55
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-26 20:03:33 +1000

    fix: replace redshift with gammastep

--- system/common/programs.nix
+++ system/common/programs.nix
@@ -6,14 +6,6 @@
   # Add binaries to PATH
   environment.localBinInPath = true;
 
-  services.redshift = {
-    enable = true;
-    temperature = {
-      day = 5500;
-      night = 3700;
-    };
-  };
-
   services.geoclue2.enable = true;
   location.provider = "geoclue2";
 }
