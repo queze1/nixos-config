fe9e6e21c1a4ff3d0f9890248c146b88b062807e
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-26 19:51:36 +1000

    feat: add redshift

--- system/common/programs.nix
+++ system/common/programs.nix
@@ -5,4 +5,15 @@
 
   # Add binaries to PATH
   environment.localBinInPath = true;
+
+  services.redshift = {
+    enable = true;
+    temperature = {
+      day = 5500;
+      night = 3700;
+    };
+  };
+
+  services.geoclue2.enable = true;
+  location.provider = "geoclue2";
 }
