446353effc630243f77ecc23b469ee7b09badb56
queze1 <52340127+queze1@users.noreply.github.com>
2026-04-27 13:51:00 +1000

    feat: add noctalia subsitutor

--- system/core/nix.nix
+++ system/core/nix.nix
@@ -12,9 +12,11 @@
   nix.settings = {
     substituters = [
       "https://nix-on-droid.cachix.org"
+      "https://noctalia.cachix.org"
     ];
     trusted-public-keys = [
       "nix-on-droid.cachix.org-1:56snoMJTXmDRC1Ei24CmKoUqvHJ9XCp+nidK7qkMQrU="
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
     ];
   };
 
