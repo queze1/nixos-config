ff91cbdea56ca778ee2df172c549398db8055a28
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-01 15:03:56 +1000

    docs: remove redundant code

--- home/modules/programs/nvf/default.nix
+++ home/modules/programs/nvf/default.nix
@@ -182,15 +182,7 @@ in
         };
 
         # Session and project management
-        session.nvim-session-manager = {
-          enable = true;
-          # May cause confusion as both project.nvim and session-manager can open a selection menu for sessions/projects
-          # By default, loadSession is overwritten
-          # mappings = {
-          #   loadSession = "<leader>ss";
-          #   loadLastSession = "<leader>sl";
-          # };
-        };
+        session.nvim-session-manager.enable = true;
         projects.project-nvim = {
           enable = true;
           setupOpts = {
