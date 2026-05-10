e8a84fe712f60aaf0814e78dad32a924b0d9c58d
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-10 17:01:40 +1000

    feat: add search engines and keybind to qute

--- home/modules/programs/qutebrowser/default.nix
+++ home/modules/programs/qutebrowser/default.nix
@@ -2,5 +2,17 @@
 {
   programs.qutebrowser = {
     enable = true;
+    searchEngines = {
+      "DEFAULT" = "https://www.google.com/search?q={}";
+      "np" = "https://search.nixos.org/packages?channel=unstable&query={}";
+      "no" = "https://search.nixos.org/options?channel=unstable&query={}";
+      "nho" = "https://home-manager-options.extranix.com/?query={}";
+      "nw" = "https://wiki.nixos.org/w/index.php?search={}";
+    };
+    keyBindings = {
+      normal = {
+        "<Esc>" = "jseval -q document.activeElement.blur()";
+      };
+    };
   };
 }
