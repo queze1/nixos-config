d367853799c4d8024cee61c5515fad63fe273216
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-15 08:20:11 +1000

    fix: infinite recursion

--- modules/flake-parts.nix
+++ modules/flake-parts.nix
@@ -1,4 +1,10 @@
+{ inputs, ... }:
 {
+  # Import home-manager's flake module
+  imports = [
+    inputs.home-manager.flakeModules.home-manager
+  ];
+
   systems = [
     "x86_64-linux"
     "x86_64-darwin"
