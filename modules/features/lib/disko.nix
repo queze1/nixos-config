1729783d691e590692a7d30d85ab4a7f0072caa2
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-14 13:07:11 +1000

    fix: fix mkif in disko

--- modules/features/lib/disko.nix
+++ modules/features/lib/disko.nix
@@ -30,38 +30,36 @@
       };
 
       config = lib.mkMerge [
-        (
-          lib.mkIf cfg.profile == "simple-efi" {
-            disko.devices.disk.main = {
-              device = config.customOptions.disko.device;
-              type = "disk";
-              content = {
-                type = "gpt";
-                partitions = {
-                  # 500M boot
-                  ESP = {
-                    type = "EF00";
-                    size = "500M";
-                    content = {
-                      type = "filesystem";
-                      format = "vfat";
-                      mountpoint = "/boot";
-                      mountOptions = [ "umask=0077" ];
-                    };
+        (lib.mkIf (cfg.profile == "simple-efi") {
+          disko.devices.disk.main = {
+            device = config.customOptions.disko.device;
+            type = "disk";
+            content = {
+              type = "gpt";
+              partitions = {
+                # 500M boot
+                ESP = {
+                  type = "EF00";
+                  size = "500M";
+                  content = {
+                    type = "filesystem";
+                    format = "vfat";
+                    mountpoint = "/boot";
+                    mountOptions = [ "umask=0077" ];
                   };
-                  root = {
-                    size = "100%";
-                    content = {
-                      type = "filesystem";
-                      format = "ext4";
-                      mountpoint = "/";
-                    };
+                };
+                root = {
+                  size = "100%";
+                  content = {
+                    type = "filesystem";
+                    format = "ext4";
+                    mountpoint = "/";
                   };
                 };
               };
             };
-          }
-        )
+          };
+        })
       ];
     };
 }
