e78db0ef3741c8e419a1067f9d0c20a0571456cc
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-14 17:17:56 +1000

    refactor: move networking, delete old files

--- modules/features/system/minimal/networking.nix
+++ modules/features/system/minimal/networking.nix
@@ -1,8 +1,47 @@
 {
   flake.nixosModules.minimalSystem =
-    { ... }:
     {
-      # TODO: Handle firewall, Tailscale SSH, depending on profile
-      networking.networkmanager.enable = true;
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      config = lib.mkMerge [
+        {
+          networking.networkmanager.enable = true;
+        }
+
+        lib.mkIf
+        (config.host.profile == "personal-machine")
+        {
+          # https://wiki.nixos.org/wiki/Tailscale
+          services.tailscale.enable = true;
+          networking.nftables.enable = true;
+          networking.firewall = {
+            enable = true;
+            trustedInterfaces = [ "tailscale0" ];
+            allowedUDPPorts = [ config.services.tailscale.port ];
+          };
+          systemd.services.tailscaled.serviceConfig.Environment = [
+            "TS_DEBUG_FIREWALL_MODE=nftables"
+          ];
+          systemd.network.wait-online.enable = false;
+          boot.initrd.systemd.network.wait-online.enable = false;
+
+          # https://discourse.nixos.org/t/how-to-configure-and-use-proton-vpn-on-nixos/65837
+          networking.firewall.checkReversePath = "loose";
+          environment.systemPackages = with pkgs; [
+            wireguard-tools
+            proton-vpn
+          ];
+        }
+
+        lib.mkIf
+        (config.host.profile == "home-server")
+        {
+          # TODO: Firewall, Tailscale SSH, hardening
+        }
+      ];
     };
 }
