92a4aa7b4138e9b427bbbf12dca697f04ad7e36c
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-14 17:42:30 +1000

    fix: fix infinite recursion and incorrect options errors

--- modules/features/system/minimal/networking.nix
+++ modules/features/system/minimal/networking.nix
@@ -12,36 +12,36 @@
           networking.networkmanager.enable = true;
         }
 
-        lib.mkIf
-        (config.host.profile == "personal-machine")
-        {
-          # https://wiki.nixos.org/wiki/Tailscale
-          services.tailscale.enable = true;
-          networking.nftables.enable = true;
-          networking.firewall = {
-            enable = true;
-            trustedInterfaces = [ "tailscale0" ];
-            allowedUDPPorts = [ config.services.tailscale.port ];
-          };
-          systemd.services.tailscaled.serviceConfig.Environment = [
-            "TS_DEBUG_FIREWALL_MODE=nftables"
-          ];
-          systemd.network.wait-online.enable = false;
-          boot.initrd.systemd.network.wait-online.enable = false;
+        # lib.mkIf
+        # (config.host.profile == "personal-machine")
+        # {
+        #   # https://wiki.nixos.org/wiki/Tailscale
+        #   services.tailscale.enable = true;
+        #   networking.nftables.enable = true;
+        #   networking.firewall = {
+        #     enable = true;
+        #     trustedInterfaces = [ "tailscale0" ];
+        #     allowedUDPPorts = [ config.services.tailscale.port ];
+        #   };
+        #   systemd.services.tailscaled.serviceConfig.Environment = [
+        #     "TS_DEBUG_FIREWALL_MODE=nftables"
+        #   ];
+        #   systemd.network.wait-online.enable = false;
+        #   boot.initrd.systemd.network.wait-online.enable = false;
 
-          # https://discourse.nixos.org/t/how-to-configure-and-use-proton-vpn-on-nixos/65837
-          networking.firewall.checkReversePath = "loose";
-          environment.systemPackages = with pkgs; [
-            wireguard-tools
-            proton-vpn
-          ];
-        }
+        #   # https://discourse.nixos.org/t/how-to-configure-and-use-proton-vpn-on-nixos/65837
+        #   networking.firewall.checkReversePath = "loose";
+        #   environment.systemPackages = with pkgs; [
+        #     wireguard-tools
+        #     proton-vpn
+        #   ];
+        # }
 
-        lib.mkIf
-        (config.host.profile == "home-server")
-        {
-          # TODO: Firewall, Tailscale SSH, hardening
-        }
+        # lib.mkIf
+        # (config.host.profile == "home-server")
+        # {
+        #   # TODO: Firewall, Tailscale SSH, hardening
+        # }
       ];
     };
 }
--- modules/features/system/standard/printing.nix
+++ modules/features/system/standard/printing.nix
@@ -2,7 +2,7 @@
   flake.nixosModules.standardSystem =
     { config, lib, ... }:
     {
-      config = lib.mkIf (config.host.isGuest == false) {
+      config = lib.mkIf (config.host.hypervisor.isGuest == false) {
         services.printing.enable = true;
         services.avahi = {
           enable = true;
