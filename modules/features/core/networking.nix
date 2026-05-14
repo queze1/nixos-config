d1d6460114e5d34eb1701657c35a232e82b181f6
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-14 15:48:35 +1000

    fix: disable openssh

--- modules/features/core/networking.nix
+++ modules/features/core/networking.nix
@@ -5,19 +5,20 @@
       # TODO: Handle firewall, Tailscale SSH, depending on profile
       networking.networkmanager.enable = true;
 
-      services.openssh = {
-        enable = true;
-        settings = {
-          PasswordAuthentication = false;
-          KbdInteractiveAuthentication = false;
-          PermitRootLogin = "no";
-        };
-        hostKeys = [
-          {
-            path = "/etc/ssh/ssh_host_ed25519_key";
-            type = "ed25519";
-          }
-        ];
-      };
+      # Disable on personal machines, insecure
+      # services.openssh = {
+      #   enable = true;
+      #   settings = {
+      #     PasswordAuthentication = false;
+      #     KbdInteractiveAuthentication = false;
+      #     PermitRootLogin = "no";
+      #   };
+      #   hostKeys = [
+      #     {
+      #       path = "/etc/ssh/ssh_host_ed25519_key";
+      #       type = "ed25519";
+      #     }
+      #   ];
+      # };
     };
 }
