ecd517557e8ff410bfa12cc253da5850b916a4a1
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-14 12:54:55 +1000

    feat: add options for disko module

--- modules/features/lib/disko.nix
+++ modules/features/lib/disko.nix
@@ -1,41 +1,67 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 {
-  flake.nixosModules = {
-    disko = {
-      # TODO: Create options to import a disko config, defaults based on profile
+  flake.nixosModules.disko =
+    { config, ... }:
+    let
+      cfg = config.customOptions.disko;
+    in
+    {
       imports = [ inputs.disko.nixosModules.default ];
-    };
 
-    # TODO: Create disko config for impermanence
-    diskoConfigVdaSimple = {
-      disko.devices.disk.main = {
-        device = "/dev/vda";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            # 500M boot
-            ESP = {
-              type = "EF00";
-              size = "500M";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
-            };
-          };
+      options.customOptions.disko = {
+        profile = lib.mkOption {
+          type = lib.types.enum [
+            "simple-efi"
+            # TODO: Create disko config for impermanence
+          ];
+          default = "simple-efi";
+          description = "Select disk config";
         };
+        device = lib.mkOption {
+          type = lib.types.str;
+          default =
+            if config.host.hypervisor.type == "utm" then
+              "/dev/vda"
+            else if config.host.hypervisor.type == "vmware" then
+              "/dev/sda"
+            else
+              "/dev/nvme0n1";
+        };
+
+        config = lib.mkMerge [
+          (
+            lib.mkIf cfg.profile == "simple-efi" {
+              disko.devices.disk.main = {
+                device = config.customOptions.disko.device;
+                type = "disk";
+                content = {
+                  type = "gpt";
+                  partitions = {
+                    # 500M boot
+                    ESP = {
+                      type = "EF00";
+                      size = "500M";
+                      content = {
+                        type = "filesystem";
+                        format = "vfat";
+                        mountpoint = "/boot";
+                        mountOptions = [ "umask=0077" ];
+                      };
+                    };
+                    root = {
+                      size = "100%";
+                      content = {
+                        type = "filesystem";
+                        format = "ext4";
+                        mountpoint = "/";
+                      };
+                    };
+                  };
+                };
+              };
+            }
+          )
+        ];
       };
     };
-  };
 }
