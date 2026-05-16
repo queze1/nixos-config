47633467d4785d64ec9a7204570569628c2e1ba6
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-16 17:14:06 +1000

    fix: move syncthing into proper place in config

--- modules/hosts/able-archer/configuration.nix
+++ modules/hosts/able-archer/configuration.nix
@@ -7,9 +7,33 @@
       self.nixosModules.queze
       self.nixosModules.standardSystem
     ];
-    host.hypervisor.type = "utm";
 
+    host.hypervisor.type = "utm";
     networking.hostName = "able-archer";
     system.stateVersion = "25.11";
+
+    # Sync with phone home server
+    services.syncthing = {
+      enable = true;
+      settings = {
+        devices = {
+          "poco-x3-pro" = {
+            id = "CGN4GSA-JX3232W-WM5XXI6-RKU3W6F-RVAZH7N-YPOCAF3-52SRDUO-HHRFFQI";
+          };
+        };
+        folders = {
+          "SillyTavern Data" = {
+            id = "nicrf-adfwa";
+            path = "/mnt/utm/Apps/SillyTavern-Launcher/SillyTavern/data/default-user";
+            devices = [ "poco-x3-pro" ];
+          };
+          "Music" = {
+            id = "ft74r-2c4sc";
+            path = "/mnt/utm/Music";
+            devices = [ "poco-x3-pro" ];
+          };
+        };
+      };
+    };
   };
 }
