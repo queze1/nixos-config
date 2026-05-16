108b3526ce515e15fbd27cd7b5fd9051d527c2fa
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-16 18:55:16 +1000

    refactor: move basic programs

--- flake.nix
+++ flake.nix
@@ -2,6 +2,8 @@
   # TODO: Import inputs from a /inputs folder
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
+
     home-manager.url = "github:nix-community/home-manager";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
--- modules/features/programs/default.nix
+++ modules/features/programs/default.nix
@@ -7,15 +7,58 @@
     home-manager.sharedModules = [ self.homeModules.allPrograms ];
   };
 
-  flake.homeModules.allPrograms = {
-    imports = [
-      self.homeModules.firefox
-      self.homeModules.imv
-      self.homeModules.qutebrowser
-      self.homeModules.vesktop
-      self.homeModules.yazi
-    ];
-  };
+  # TODO: Configure pkgs-stable
+  flake.homeModules.allPrograms =
+    { pkgs, ... }:
+    {
+      imports = [
+        self.homeModules.firefox
+        self.homeModules.imv
+        self.homeModules.qutebrowser
+        self.homeModules.vesktop
+        self.homeModules.yazi
+      ];
+
+      home.packages = with pkgs; [
+        # TODO: Use overlay
+        (obsidian.override {
+          commandLineArgs = "--ozone-platform=x11";
+        })
+        # Force Audacity to use native Wayland
+        (symlinkJoin {
+          name = "audacity-wayland-fix";
+          paths = [ audacity ];
+          nativeBuildInputs = [ makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/audacity \
+              --set GDK_BACKEND wayland
+          '';
+        })
+
+        digikam
+        gnome-clocks
+        kdePackages.okular
+        keepassxc
+        obs-studio
+        pinta
+        # pkgs-stable.celluloid
+        # pkgs-stable.qimgv
+        qalculate-qt
+
+        # CLI tools
+        clipboard-jh
+        fastfetch
+        ffmpeg
+        # pkgs-stable.yt-dlp
+        pywalfox-native
+        xclip # for X11-Wayland sync on VMs
+
+        # AI tools
+        cursor-cli
+        codex
+        github-copilot-cli
+      ];
+    };
 
   flake.nixosModules.minimalPrograms = {
     imports = [ self.nixosModules.fish ];
@@ -23,8 +66,19 @@
     home-manager.sharedModules = [ self.homeModules.minimalPrograms ];
   };
 
-  flake.homeModules.minimalPrograms = [
-    self.homeModules.git
-    self.homeModules.nvf
-  ];
+  flake.homeModules.minimalPrograms =
+    { pkgs, ... }:
+    {
+      imports = [
+        self.homeModules.git
+        self.homeModules.nvf
+      ];
+
+      home.packages = with pkgs; [
+        sshfs
+        tree
+        unzip
+        wl-clipboard
+      ];
+    };
 }
