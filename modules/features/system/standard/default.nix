d73a5432791381aae5f79d3fdc00b3add8e073ea
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-16 15:35:07 +1000

    feat: add nix-ld to standard system

--- modules/features/system/standard/default.nix
+++ modules/features/system/standard/default.nix
@@ -3,4 +3,8 @@
   flake.nixosModules.standardSystem = {
     imports = [ self.nixosModules.minimalSystem ];
   };
+
+  # Run unpackaged binaries
+  programs.nix-ld.enable = true;
+  environment.localBinInPath = true;
 }
