ee03a74374131899dd301b3be193a6aea4074b38
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-17 00:21:49 +1000

    fix: use home manager for pfp

--- modules/features/desktop/noctalia.nix
+++ modules/features/desktop/noctalia.nix
@@ -3,21 +3,26 @@
   ...
 }:
 {
-  flake.homeModules.noctalia = {
-    imports = [
-      inputs.noctalia.homeModules.default
-    ];
+  flake.homeModules.noctalia =
+    { config, ... }:
+    {
+      imports = [
+        inputs.noctalia.homeModules.default
+      ];
 
-    home.shellAliases = {
-      # TODO: Get the directory of this file and then put the result in that directory
-      noctalia-export = "noctalia-shell ipc call state all | nix run nixpkgs#jq .settings > ~/etc/nixos/home/modules/desktop/noctalia.json";
-    };
+      home.shellAliases = {
+        # TODO: Get the directory of this file and then put the result in that directory
+        noctalia-export = "noctalia-shell ipc call state all | nix run nixpkgs#jq .settings > ~/etc/nixos/home/modules/desktop/noctalia.json";
+      };
 
-    programs.noctalia-shell = {
-      enable = true;
-      # TODO: Selectively override settings which should be dynamic
-      # TODO: Use Home Manager to set default profile picture & wallpaper
-      settings = builtins.fromJSON (builtins.readFile ./noctalia.json);
+      home.file.profilePicture = {
+        target = "${config.home.homeDirectory}/.face";
+      };
+
+      programs.noctalia-shell = {
+        enable = true;
+        # TODO: Use Home Manager to set default profile picture & wallpaper
+        settings = builtins.fromJSON (builtins.readFile ./noctalia.json);
+      };
     };
-  };
 }
