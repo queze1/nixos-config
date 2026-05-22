5e297f3ceef74cdae340f7eb44ef042468d69b79
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-22 18:25:47 +0800

    chore: update time zone

--- modules/features/system/minimal/localisation.nix
+++ modules/features/system/minimal/localisation.nix
@@ -1,6 +1,6 @@
 {
   flake.nixosModules.localisation = {
-    time.timeZone = "Australia/Sydney";
+    time.timeZone = "Asia/Shanghai";
 
     i18n.defaultLocale = "en_GB.UTF-8";
     i18n.extraLocaleSettings = {
