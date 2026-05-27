f7dae99358810a4c61d3a06964e4bd2420fcf8fe
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-27 12:17:10 +1000

    refactor: clean up flake.nix

--- flake.nix
+++ flake.nix
@@ -1,39 +1,41 @@
 {
-  # TODO: Import inputs from a /inputs folder
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
+
   inputs = {
+    # Nix ecosystem
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
-
     home-manager.url = "github:nix-community/home-manager";
 
+    # Flake frameworks
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
-
     wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
 
+    # Libraries
     agenix = {
       url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
+    deploy-rs.url = "github:serokell/deploy-rs";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     preservation.url = "github:nix-community/preservation";
 
+    # Applications
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     nvf = {
       url = "github:notashelf/nvf";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+    };
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
 
     # Yazi plugins
@@ -46,14 +48,11 @@
       flake = false;
     };
 
-    deploy-rs.url = "github:serokell/deploy-rs";
-
+    # Other
     nix-on-droid-repo = {
       url = "github:queze1/nix-on-droid-config";
       inputs.nixpkgs.follows = "nixpkgs-stable";
       inputs.nixpkgs-unstable.follows = "nixpkgs";
     };
   };
-
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 }
