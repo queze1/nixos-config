603fc3743ae470b7ad6203acf2ddb9ad7e2b9eee
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-29 03:58:18 +1000

    fix: fix location of home manager in commander

--- modules/users/commander.nix
+++ modules/users/commander.nix
@@ -13,15 +13,14 @@ in
           "networkmanager"
           "wheel"
         ];
-
-        home-manager.users.${username} = {
-          home.username = username;
-          home.homeDirectory = "/home/${username}";
-          home.stateVersion = "25.11";
-          programs.home-manager.enable = true;
-        };
-
         openssh.authorizedKeys.keys = sshKeys.allKeys;
+      };
+
+      home-manager.users.${username} = {
+        home.username = username;
+        home.homeDirectory = "/home/${username}";
+        home.stateVersion = "25.11";
+        programs.home-manager.enable = true;
       };
     };
 }
