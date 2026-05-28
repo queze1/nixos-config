31b7f8c10c8c8bc31691d2de15807ed7a6429a35
queze <52340127+queze1@users.noreply.github.com>
2026-05-28 13:32:59 +1000

    feat: print the command run in install.sh

--- install.sh
+++ install.sh
@@ -13,9 +13,17 @@ fi
 TARGET_IP=$1
 HOSTNAME=${2:-$DEFAULT_HOSTNAME}
 
+# Remember to update this whenever the actual command is updated
+cat <<EOF
+Running: nix run github:nix-community/nixos-anywhere -- \\
+    --flake ".#$HOSTNAME" \\
+    --copy-host-keys \\
+    --build-on remote \\
+    "root@$TARGET_IP"
+EOF
+
 nix run github:nix-community/nixos-anywhere -- \
     --flake ".#$HOSTNAME" \
     --copy-host-keys \
     --build-on remote \
     "root@$TARGET_IP"
-
