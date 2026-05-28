ad9b2080ea73ce1a0bf4510a0aeb9de6e2475a33
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-28 17:52:09 +1000

    fix: add persistent in identity paths, copy key to persistent

--- install.sh
+++ install.sh
@@ -4,21 +4,26 @@ set -e
 
 DEFAULT_HOSTNAME="able-archer"
 DEFAULT_HOST_KEY_PATH="secrets/tmp/ssh_host_ed25519_key"
+DEFAULT_COPY_TO_PERSISTENT=1
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 <target-ip> [hostname] [ssh-host-key-path]"
-    echo "Example: $0 192.168.1.100 $DEFAULT_HOSTNAME $DEFAULT_HOST_KEY_PATH"
+    echo "Usage: $0 <target-ip> [hostname] [ssh-host-key-path] [copy-to-persistent]"
+    echo "Example: $0 192.168.1.100 $DEFAULT_HOSTNAME $DEFAULT_HOST_KEY_PATH $DEFAULT_COPY_TO_PERSISTENT"
     exit 1
 fi
 
 TARGET_IP=$1
 HOSTNAME=${2:-$DEFAULT_HOSTNAME}
 HOST_KEY_PATH=${3:-$DEFAULT_HOST_KEY_PATH}
+COPY_TO_PERSISTENT=${4:-$DEFAULT_COPY_TO_PERSISTENT}
 HOST_KEY_NAME=$(basename "$HOST_KEY_PATH")
 HOST_KEY_DIR=$(dirname "$HOST_KEY_PATH")
+
+# Temporary directories
 TMP_DIR="secrets/tmp"
 STAGING_DIR="$TMP_DIR/staging"
 STAGING_ETC_SSH_DIR="$STAGING_DIR/etc/ssh"
+STAGING_PERSISTENT_ETC_SSH_DIR="$STAGING_DIR/persistent/etc/ssh"
 
 # Helper to echo a command and then execute it
 run_cmd() {
@@ -54,6 +59,11 @@ fi
 
 run_cmd "mkdir -p \"$STAGING_ETC_SSH_DIR\""
 run_cmd "cp \"$HOST_KEY_PATH\" \"$STAGING_ETC_SSH_DIR/$HOST_KEY_NAME\""
+
+if [ "$COPY_TO_PERSISTENT" = "1" ]; then
+    run_cmd "mkdir -p \"$STAGING_PERSISTENT_ETC_SSH_DIR\""
+    run_cmd "cp \"$HOST_KEY_PATH\" \"$STAGING_PERSISTENT_ETC_SSH_DIR/$HOST_KEY_NAME\""
+fi
 
 # Remember to update this whenever the command is updated
 cat <<EOF
--- modules/features/system/minimal/lib/agenix.nix
+++ modules/features/system/minimal/lib/agenix.nix
@@ -13,7 +13,10 @@
       imports = [ inputs.agenix.nixosModules.default ];
 
       environment.systemPackages = [ inputs'.agenix.packages.default ];
-      age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      age.identityPaths = [
+        "/etc/ssh/ssh_host_ed25519_key"
+        "/persistent/etc/ssh/ssh_host_ed25519_key"
+      ];
 
       home-manager.sharedModules = [ self.homeModules.agenix ];
     }
