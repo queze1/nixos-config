8a5988908bebeea080e556eb15723df7eb27c590
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-30 09:54:20 +1000

    chore: add todos to use nix factor

--- modules/hosts/steadfast-dart/default.nix
+++ modules/hosts/steadfast-dart/default.nix
@@ -1,6 +1,7 @@
 { self, inputs, ... }:
 {
   # For fastest home server
+  # TODO: Use nix-factor for home servers
   flake.nixosConfigurations.steadfast-dart = inputs.nixpkgs.lib.nixosSystem {
     modules = [
       self.nixosModules.steadfastDartConfiguration
--- modules/hosts/steadfast-noon/default.nix
+++ modules/hosts/steadfast-noon/default.nix
@@ -1,5 +1,6 @@
 { self, inputs, ... }:
 {
+  # TODO: Use nix-factor for home servers
   flake.nixosConfigurations.steadfast-noon = inputs.nixpkgs.lib.nixosSystem {
     modules = [
       self.nixosModules.steadfastNoonConfiguration
