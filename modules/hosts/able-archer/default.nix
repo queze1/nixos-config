55d470c5d5723be0784a135a7ee837d6b4d3e0bb
queze1 <52340127+queze1@users.noreply.github.com>
2026-05-30 10:41:12 +1000

    chore: add explanation commit for pkgs-stable

--- modules/hosts/able-archer/default.nix
+++ modules/hosts/able-archer/default.nix
@@ -8,6 +8,7 @@
   flake.nixosConfigurations.able-archer = withSystem "aarch64-linux" (
     { self', ... }:
     inputs.nixpkgs.lib.nixosSystem {
+      # Allow NixOS modules to access pkgs-stable
       specialArgs = {
         pkgs-stable = self'.legacyPackages.pkgs-stable;
       };
