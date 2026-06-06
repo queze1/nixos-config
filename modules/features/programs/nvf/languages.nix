e663142f3a7190242ddf431a684a4b090ee03f4a
queze1 <52340127+queze1@users.noreply.github.com>
2026-06-06 11:48:04 +1000

    refactor: move out nvf language support

---
+++ modules/features/programs/nvf/languages.nix
@@ -0,0 +1,84 @@
+{
+  flake.homeModules.nvf =
+    {
+
+      lib,
+      pkgs,
+      ...
+    }:
+    let
+      inherit (lib.generators) mkLuaInline;
+    in
+    {
+      programs.nvf.settings.vim = {
+        languages = {
+          java.enable = true;
+          markdown = {
+            enable = true;
+            extensions.render-markdown-nvim.enable = true;
+          };
+          nix = {
+            enable = true;
+            lsp.servers = [ "nixd" ];
+          };
+          python = {
+            enable = true;
+            # We manually configure Python formatting elsewhere
+            format.enable = false;
+          };
+          rust.enable = true;
+          typescript.enable = true;
+        };
+
+        # Autoformat on save
+        formatter.conform-nvim = {
+          enable = true;
+          setupOpts = {
+            formatters_by_ft = {
+              # Uses ruff in PATH
+              python = [
+                "ruff_fix"
+                "ruff_format"
+                "ruff_organize_imports"
+              ];
+            };
+          };
+        };
+
+        lsp = {
+          enable = true;
+          lspconfig.enable = true;
+          formatOnSave = true;
+          servers = {
+            basedpyright = {
+              settings = {
+                basedpyright = {
+                  disableOrganizeImports = true;
+                };
+              };
+
+              # Replace commands created by nvf
+              # LspPyrightOrganizeImports: made redundant by ruff
+              # LspPyrightSetPythonPath: made redundant by direnv
+              on_attach = lib.mkForce (mkLuaInline ''
+                function(client, bufnr)
+                  vim.api.nvim_buf_create_user_command(bufnr, 'LspPyrightWriteBaseline', function()
+                    vim.fn.jobstart({ "${lib.getExe pkgs.basedpyright}", "--writebaseline" }, {
+                      cwd = client.config.root_dir,
+                      on_exit = function(_, code)
+                        if code == 0 then
+                          vim.notify("basedpyright: baseline written", vim.log.levels.INFO)
+                        else
+                          vim.notify("basedpyright: baseline failed", vim.log.levels.ERROR)
+                        end
+                      end
+                    })
+                  end, { desc = 'Run basedpyright --writebaseline' })
+                end
+              '');
+            };
+          };
+        };
+      };
+    };
+}
