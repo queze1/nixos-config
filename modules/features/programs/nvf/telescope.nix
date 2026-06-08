ca0d5a036270e3a8afc3bfac7a29f0b5960c8d02
queze1 <52340127+queze1@users.noreply.github.com>
2026-06-08 11:12:38 +1000

    fix: use telescope cmd to run live grep args

--- modules/features/programs/nvf/telescope.nix
+++ modules/features/programs/nvf/telescope.nix
@@ -44,7 +44,7 @@
         {
           key = "<Leader>fg";
           mode = "n";
-          action = "<cmd>lua require(\"telescope\").extensions.live_grep_args.live_grep_args()<CR>";
+          action = "<cmd>Telescope live_grep_args<CR>";
           desc = "Live Grep (args) [Telescope]";
           silent = true;
         }
