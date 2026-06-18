ff31e9618bd0489364cf719b085bd76d9749c3b5
queze1 <52340127+queze1@users.noreply.github.com>
2026-06-19 09:08:46 +1000

    feat: attempt to add pkgs-stable to steadfast-base

--- modules/hosts/steadfast-base/default.nix
+++ modules/hosts/steadfast-base/default.nix
@@ -1,6 +1,10 @@
-{self, ...}: {
+{
+  self,
+  inputs,
+  ...
+}: {
   # Base configuration for home servers
-  flake.nixosModules.steadfastBase = {
+  flake.nixosModules.steadfastBase = {pkgs, ...}: {
     imports = [
       self.nixosModules.coreFeatures
       self.nixosModules.minimalPrograms
@@ -12,6 +16,8 @@
       disko.profile = "hybrid-tmpfs-on-root";
       preservation.enable = true;
     };
+
+    _module.args.pkgs-stable = inputs.nixpkgs-stable.legacyPackages.${pkgs.system};
 
     system.stateVersion = "25.11";
   };
