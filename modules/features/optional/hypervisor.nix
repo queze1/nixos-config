b886d8517dc962db0134a2e685b75b3f22c5b66b
queze1 <52340127+queze1@users.noreply.github.com>
2026-06-18 14:13:54 +1000

    style: collapse option definition for hypervisor

--- modules/features/optional/hypervisor.nix
+++ modules/features/optional/hypervisor.nix
@@ -6,39 +6,37 @@
   }: let
     cfg = config.host.hypervisor;
   in {
-    options.host = {
-      hypervisor = {
-        type = lib.mkOption {
-          type = lib.types.enum [
-            "utm"
-            "vmware"
-            "none"
-          ];
-          default = "none";
-        };
+    options.host.hypervisor = {
+      type = lib.mkOption {
+        type = lib.types.enum [
+          "utm"
+          "vmware"
+          "none"
+        ];
+        default = "none";
+      };
 
-        isGuest = lib.mkOption {
-          type = lib.types.bool;
-          default = config.host.hypervisor.type != "none";
-          readOnly = true;
-        };
+      isGuest = lib.mkOption {
+        type = lib.types.bool;
+        default = config.host.hypervisor.type != "none";
+        readOnly = true;
+      };
 
-        sharedFolder = lib.mkOption {
-          type = lib.types.nullOr lib.types.str;
-          default =
-            if config.host.hypervisor.type == "utm"
-            then "/mnt/utm"
-            else if config.host.hypervisor.type == "vmware"
-            then "/mnt/hgfs"
-            else null;
-          description = "Where to mount the shared folder";
-        };
+      sharedFolder = lib.mkOption {
+        type = lib.types.nullOr lib.types.str;
+        default =
+          if config.host.hypervisor.type == "utm"
+          then "/mnt/utm"
+          else if config.host.hypervisor.type == "vmware"
+          then "/mnt/hgfs"
+          else null;
+        description = "Where to mount the shared folder";
+      };
 
-        useForXDGUserDirs = lib.mkOption {
-          type = lib.types.bool;
-          default = true;
-          description = "Whether to place XDG user directories (e.g. Downloads, Documents) in the shared folder";
-        };
+      useForXDGUserDirs = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Whether to place XDG user directories (e.g. Downloads, Documents) in the shared folder";
       };
     };
 
