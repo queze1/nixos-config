22725a23fcdf0931f61d5db7c598a01c5e03f9ba
queze1 <52340127+queze1@users.noreply.github.com>
2026-06-18 15:48:41 +1000

    chore: add ssh key for steadfast noon

--- ssh-keys.nix
+++ ssh-keys.nix
@@ -1,7 +1,9 @@
 rec {
   ableArcher = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINC3vA0PnFXyFRgitP7U8PL+SlTvqvE6eY73rpW5Rj4y";
+  steadfastNoon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBtTfN1JOki9MUzxZTZ3iKmvRf/9lCvyyL3dV0vxLqww";
 
   allKeys = [
     ableArcher
+    steadfastNoon
   ];
 }
