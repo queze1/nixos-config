960a81329e40f72232f8851a93e27fdff66b629a
queze1 <52340127+queze1@users.noreply.github.com>
2026-06-19 10:01:35 +1000

    fix: use legacy version of noctalia

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
 
     # Applications
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
+      url = "github:noctalia-dev/noctalia/legacy-v4";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nvf = {
