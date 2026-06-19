93cfe525ecab551a12cfcb177a74610dd1d2b966
queze1 <52340127+queze1@users.noreply.github.com>
2026-06-19 17:38:58 +1000

    feat: add vesktop themes

--- modules/features/programs/vesktop/default.nix
+++ modules/features/programs/vesktop/default.nix
@@ -1,10 +1,25 @@
-{
+{self, ...}: let
+  # Dynamically find themes in assets/themes
+  themesDir = "${self}/assets/themes";
+  themeDirContents = builtins.readDir themesDir;
+
+  themeNames =
+    builtins.filter
+    (name: themeDirContents.${name} == "regular")
+    (builtins.attrNames themeDirContents);
+
+  themeSet = builtins.listToAttrs (map (name: {
+      name = name;
+      value = builtins.readFile "${themesDir}/${name}";
+    })
+    themeNames);
+in {
   flake.homeModules.vesktop = {
     programs.vesktop = {
       enable = true;
-      # TODO: Add themes from GitHub repo and delete GitHub repo
       vencord = {
         settings = builtins.fromJSON (builtins.readFile ./settings.json);
+        themes = themeSet;
       };
     };
   };
