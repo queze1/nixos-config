8b78401f45bdc559a21fe2a1aa426479b386dff9
queze1 <52340127+queze1@users.noreply.github.com>
2026-06-23 11:20:43 +1000

    fix: don't run nix flake check

--- devenv.nix
+++ devenv.nix
@@ -3,10 +3,5 @@
     alejandra.enable = true;
     deadnix.enable = true;
     flake-checker.enable = true;
-    my-flake-checker = {
-      enable = true;
-      entry = "nix flake check";
-      pass_filenames = false;
-    };
   };
 }
