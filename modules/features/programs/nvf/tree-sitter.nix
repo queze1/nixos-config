232cdd877effad23576df3fba62d35e355b82cab
queze1 <52340127+queze1@users.noreply.github.com>
2026-06-30 11:48:32 +1000

    fix: change treesitter keymaps

--- modules/features/programs/nvf/tree-sitter.nix
+++ modules/features/programs/nvf/tree-sitter.nix
@@ -53,14 +53,14 @@
               require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
             end, { desc = "class [Treesitter]" })
 
-            -- m: method
-            vim.keymap.set({ "x", "o" }, "am", function()
+            -- a: argument
+            vim.keymap.set({ "x", "o" }, "aa", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@parameter.outer", "textobjects")
-            end, { desc = "method [Treesitter]" })
+            end, { desc = "argument [Treesitter]" })
 
-            vim.keymap.set({ "x", "o" }, "im", function()
+            vim.keymap.set({ "x", "o" }, "ia", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@parameter.inner", "textobjects")
-            end, { desc = "method [Treesitter]" })
+            end, { desc = "argument [Treesitter]" })
 
             -- l: loop
             vim.keymap.set({ "x", "o" }, "al", function()
