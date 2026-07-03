85b783b554a97e36130876ea454d9d0b59e3638b
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-03 10:40:09 +1000

    feat: add commit signing to git

--- modules/features/programs/git.nix
+++ modules/features/programs/git.nix
@@ -1,7 +1,12 @@
 {
-  flake.homeModules.git = {
+  flake.homeModules.git = {config, ...}: {
     programs.git = {
       enable = true;
+      signing = {
+        key = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+        signByDefault = true;
+      };
+
       settings = {
         user.name = "queze1";
         user.email = "52340127+queze1@users.noreply.github.com";
