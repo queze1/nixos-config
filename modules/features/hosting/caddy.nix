4f3b94450f0f4afe2c1978ec5e1b8c68c5fb30c2
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-09 17:23:53 +1000

    feat: implement tailscale auth

--- modules/features/hosting/caddy.nix
+++ modules/features/hosting/caddy.nix
@@ -16,11 +16,28 @@
       };
       globalConfig = ''
         acme_dns cloudflare {file.${config.age.secrets.osipol-cloudflare-api-token.path}}
+
+        (tailscale_auth) {
+          forward_auth unix//run/tailscale.nginx-auth.sock {
+            uri /auth
+            header_up Remote-Addr {remote_host}
+            header_up Remote-Port {remote_port}
+            header_up Original-URI {uri}
+            copy_headers {
+              Tailscale-User>X-Webauth-User
+              Tailscale-Name>X-Webauth-Name
+              Tailscale-Login>X-Webauth-Login
+              Tailscale-Tailnet>X-Webauth-Tailnet
+              Tailscale-Profile-Picture>X-Webauth-Profile-Picture
+            }
+          }
+        }
       '';
 
       virtualHosts = {
         "new.navidrome.osipol.uk" = {
           extraConfig = ''
+            import tailscale_auth
             reverse_proxy localhost:4533
           '';
         };
