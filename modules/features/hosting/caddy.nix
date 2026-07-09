8d9933298cf4d82f98ff6c5dbd84d4e7e598e418
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-09 16:54:34 +1000

    feat: host navidrime with caddy

--- modules/features/hosting/caddy.nix
+++ modules/features/hosting/caddy.nix
@@ -1,7 +1,30 @@
-{
+{inputs, ...}: {
   flake.nixosModules.caddy = {
+    config,
+    pkgs,
+    ...
+  }: {
+    age.secrets.osipol-cloudflare-api-token.file = "${inputs.secrets}/osipol-cloudflare-api-token.age";
+
     services.caddy = {
       enable = true;
+      package = pkgs.caddy.withPlugins {
+        plugins = [
+          "github.com/caddy-dns/cloudflare@v0.2.4"
+        ];
+        hash = "sha256-hEHgAG0F0ozHRAPuxEqLyTATBrE+pajeXDiSNwniorg=";
+      };
+      globalConfig = ''
+        acme_dns cloudflare {file.${config.age.secrets.osipol-cloudflare-api-token.path}}
+      '';
+
+      virtualHosts = {
+        "new.navidrome.osipol.uk" = {
+          extraConfig = ''
+            reverse_proxy localhost:4533
+          '';
+        };
+      };
     };
   };
 }
