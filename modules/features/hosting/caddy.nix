93334e3e2e023515037b9a2689fe703594c4aa69
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-10 10:08:10 +1000

    fix: implement tailscale auth for caddy

--- modules/features/hosting/caddy.nix
+++ modules/features/hosting/caddy.nix
@@ -1,7 +1,6 @@
 {inputs, ...}: {
   flake.nixosModules.caddy = {
     config,
-    lib,
     pkgs,
     ...
   }: {
@@ -10,6 +9,10 @@
       owner = config.services.caddy.user;
       group = config.services.caddy.group;
     };
+
+    # Required to authenticate requests with Tailscale
+    services.tailscaleAuth.enable = true;
+    users.users.caddy.extraGroups = ["tailscale-nginx-auth"];
 
     services.caddy = {
       enable = true;
@@ -21,15 +24,13 @@
         hash = "sha256-hEHgAG0F0ozHRAPuxEqLyTATBrE+pajeXDiSNwniorg=";
       };
 
-      logFormat = lib.mkForce "level INFO";
-
       globalConfig = ''
         acme_dns cloudflare {file.${config.age.secrets.osipol-cloudflare-api-token.path}}
       '';
 
       extraConfig = ''
         (tailscale_auth) {
-          forward_auth unix//run/tailscale.nginx-auth.sock {
+          forward_auth unix//${config.services.tailscaleAuth.socketPath} {
             uri /auth
             header_up Remote-Addr {remote_host}
             header_up Remote-Port {remote_port}
@@ -48,9 +49,8 @@
       virtualHosts = {
         "new.navidrome.osipol.uk" = {
           extraConfig = ''
-            # import tailscale_auth
-            respond "Hello world!"
-            # reverse_proxy localhost:4533
+            import tailscale_auth
+            reverse_proxy localhost:4533
             tls {
               resolvers 1.1.1.1
             }
