6a1d76b8e64c90f8f6896046eff4b7bf3fab6066
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-10 13:06:26 +1000

    fix: preserve navidrome data

--- modules/features/hosting/navidrome.nix
+++ modules/features/hosting/navidrome.nix
@@ -1,10 +1,20 @@
 {
-  flake.nixosModules.navidrome = {
+  flake.nixosModules.navidrome = {config, ...}: {
     services.navidrome = {
       enable = true;
       settings = {
         "Scanner.Schedule" = "0 * * * *";
       };
     };
+
+    # Preserve Navidrome data
+    my.preservation.extraDirectories = [
+      {
+        directory = "/var/lib/navidrome";
+        user = config.services.navidrome.user;
+        group = config.services.navidrome.group;
+        mode = "0700";
+      }
+    ];
   };
 }
