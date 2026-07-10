b7e672c5ed0f4d462550e54acac6a845835cc707
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-10 14:10:28 +1000

    refactor: add option to set portal for yubal

--- modules/features/hosting/yubal.nix
+++ modules/features/hosting/yubal.nix
@@ -7,76 +7,77 @@
     cfg = config.services.yubal;
   in {
     options.services.yubal = {
+      port = lib.mkOption {
+        type = lib.types.int;
+        default = 8000;
+        description = "Port to run yubal on.";
+      };
+      dataDir = lib.mkOption {
+        type = lib.types.str;
+        default = "/var/lib/yubal";
+        description = "Directory where yubal stores its data.";
+      };
       uid = lib.mkOption {
         type = lib.types.int;
         default = 980;
         description = "UID for the yubal system user.";
       };
-
       gid = lib.mkOption {
         type = lib.types.int;
         default = 980;
         description = "GID for the yubal system group.";
       };
-
-      dataDir = lib.mkOption {
-        type = lib.types.str;
-        default = "/var/lib/yubal";
-        description = "Directory where Yubal stores its data.";
-      };
     };
 
-    config = {
-      # Create a system user and group for yubal
-      users.users.yubal = {
-        isSystemUser = true;
-        uid = cfg.uid;
+    # Create a system user and group for yubal
+    users.users.yubal = {
+      isSystemUser = true;
+      uid = cfg.uid;
+      group = "yubal";
+    };
+    users.groups.yubal = {
+      gid = cfg.gid;
+    };
+
+    # Preserve yubal data
+    my.preservation.extraDirectories = [
+      {
+        directory = cfg.dataDir;
+        user = "yubal";
         group = "yubal";
-      };
-      users.groups.yubal = {
-        gid = cfg.gid;
-      };
+        mode = "0750";
+      }
+    ];
 
-      # Preserve yubal data
-      my.preservation.extraDirectories = [
-        {
-          directory = cfg.dataDir;
-          user = "yubal";
-          group = "yubal";
-          mode = "0750";
-        }
-      ];
+    # Create yubal directories
+    systemd.tmpfiles.rules = [
+      "d ${cfg.dataDir}/data   0750 yubal yubal -"
+      "d ${cfg.dataDir}/config 0750 yubal yubal -"
+    ];
 
-      # Create yubal directories
-      systemd.tmpfiles.rules = [
-        "d ${cfg.dataDir}/data   0750 yubal yubal -"
-        "d ${cfg.dataDir}/config 0750 yubal yubal -"
-      ];
+    # Allow Navidrome to access files downloaded by Yubal
+    users.users.${config.services.navidrome.user}.extraGroups = ["yubal"];
 
-      # Allow Navidrome to access files downloaded by Yubal
-      users.users.${config.services.navidrome.user}.extraGroups = ["yubal"];
+    # Run Yubal through Podman
+    virtualisation.oci-containers = {
+      backend = "podman";
+      containers.yubal = {
+        image = "ghcr.io/guillevc/yubal:latest";
+        autoStart = true;
+        ports = ["${cfg.port}:8000"];
 
-      # Run Yubal through Podman
-      virtualisation.oci-containers = {
-        backend = "podman";
-        containers.yubal = {
-          image = "ghcr.io/guillevc/yubal:latest";
-          autoStart = true;
-          ports = ["8000:8000"];
-
-          environment = {
-            PUID = toString cfg.uid;
-            PGID = toString cfg.gid;
-            YUBAL_SCHEDULER_CRON = "0 0 * * *";
-            YUBAL_DOWNLOAD_UGC = "false";
-            YUBAL_TZ = "UTC";
-          };
-
-          volumes = [
-            "${cfg.dataDir}/data:/app/data"
-            "${cfg.dataDir}/config:/app/config"
-          ];
+        environment = {
+          PUID = toString cfg.uid;
+          PGID = toString cfg.gid;
+          YUBAL_SCHEDULER_CRON = "0 0 * * *";
+          YUBAL_DOWNLOAD_UGC = "false";
+          YUBAL_TZ = "UTC";
         };
+
+        volumes = [
+          "${cfg.dataDir}/data:/app/data"
+          "${cfg.dataDir}/config:/app/config"
+        ];
       };
     };
   };
