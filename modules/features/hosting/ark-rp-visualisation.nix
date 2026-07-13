68b5fcf84efcc874985ad14e9ef93611cd1f8212
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-13 10:43:51 +1000

    fix: wrap config in config and other fix

--- modules/features/hosting/ark-rp-visualisation.nix
+++ modules/features/hosting/ark-rp-visualisation.nix
@@ -15,53 +15,55 @@
       };
     };
 
-    users.users.ark-rp-viz = {
-      isSystemUser = true;
-      group = "ark-rp-viz";
-    };
-    users.groups.ark-rp-viz = {};
-
-    age.secrets.ark-rp-visualisation-env = {
-      file = "${inputs.secrets}/ark-rp-visualisation-env.age";
-      owner = "ark-rp-viz";
-      group = "ark-rp-viz";
-    };
-
-    # Service to run ark-rp-visualisation
-    systemd.services.ark-rp-viz = {
-      description = "ARK D&D Campaign Dashboard";
-      after = ["network.target"];
-      wantedBy = ["multi-user.target"];
-
-      serviceConfig = {
-        ExecStart = "${inputs.ark-rp-visualisation.packages.${pkgs.stdenv.hostPlatform.system}.default}/bin/ark-rp-visualisation";
-        User = "ark-rp-viz";
-        Group = "ark-rp-viz";
-        Restart = "always";
-
-        Environment = {
-          PORT = toString cfg.port;
-        };
-        EnvironmentFile = config.age.secrets.ark-rp-visualisation-env.path;
-
-        # Hardening
-        ProtectSystem = "strict";
-        ProtectHome = true;
-        PrivateTmp = true;
-        NoNewPrivileges = true;
+    config = {
+      users.users.ark-rp-viz = {
+        isSystemUser = true;
+        group = "ark-rp-viz";
       };
-    };
+      users.groups.ark-rp-viz = {};
 
-    # Networking with Cloudflare tunnel
-    services.caddy.virtualHosts."http://ark-rp-visualisation.osipol.uk" = {
-      extraConfig = ''
-        # Only listen to localhost (e.g. Cloudflared tunnel)
-        bind 127.0.0.1
-        reverse_proxy localhost:${cfg.port}
-      '';
-    };
-    services.cloudflared.tunnels."b6ce003f-d222-4d1c-8e67-56ac678280ba".ingress = {
-      "ark-rp-visualisation.osipol.uk" = "http://localhost:80"; # Forward to Caddy
+      age.secrets.ark-rp-visualisation-env = {
+        file = "${inputs.secrets}/ark-rp-visualisation-env.age";
+        owner = "ark-rp-viz";
+        group = "ark-rp-viz";
+      };
+
+      # Service to run ark-rp-visualisation
+      systemd.services.ark-rp-viz = {
+        description = "ARK D&D Campaign Dashboard";
+        after = ["network.target"];
+        wantedBy = ["multi-user.target"];
+
+        serviceConfig = {
+          ExecStart = "${inputs.ark-rp-visualisation.packages.${pkgs.stdenv.hostPlatform.system}.default}/bin/ark-rp-visualisation";
+          User = "ark-rp-viz";
+          Group = "ark-rp-viz";
+          Restart = "always";
+
+          Environment = ''
+            PORT=${toString cfg.port}
+          '';
+          EnvironmentFile = config.age.secrets.ark-rp-visualisation-env.path;
+
+          # Hardening
+          ProtectSystem = "strict";
+          ProtectHome = true;
+          PrivateTmp = true;
+          NoNewPrivileges = true;
+        };
+      };
+
+      # Networking with Cloudflare tunnel
+      services.caddy.virtualHosts."http://ark-rp-visualisation.osipol.uk" = {
+        extraConfig = ''
+          # Only listen to localhost (e.g. Cloudflared tunnel)
+          bind 127.0.0.1
+          reverse_proxy localhost:${toString cfg.port}
+        '';
+      };
+      services.cloudflared.tunnels."b6ce003f-d222-4d1c-8e67-56ac678280ba".ingress = {
+        "ark-rp-visualisation.osipol.uk" = "http://localhost:80"; # Forward to Caddy
+      };
     };
   };
 }
