12e976e5767a568d9ffa89981c9a300149d6706c
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-14 13:34:54 +1000

    feat: use firebog list for pihole

--- modules/features/hosting/pihole.nix
+++ modules/features/hosting/pihole.nix
@@ -1,6 +1,17 @@
 {
-  flake.nixosModules.pihole = {config, ...}: let
+  flake.nixosModules.pihole = {
+    config,
+    lib,
+    ...
+  }: let
     cfg = config.services.pihole-ftl;
+
+    firebogListText = builtins.readFile (builtins.fetchurl {
+      url = "https://v.firebog.net/hosts/lists.php?type=tick";
+      sha256 = "sha256:1zpg9q8c4br5piznnsg3sfirsv0rqgm83kawxagzx308islnk9s1";
+    });
+    firebogListLines = lib.strings.splitString "\n" firebogListText;
+    piholeLists = map (url: {inherit url;}) firebogListLines;
   in {
     services.pihole-ftl = {
       enable = true;
@@ -18,11 +29,7 @@
         };
       };
       # Has bug where setup service will try to add a list even if it already exists, causing an error
-      # lists = [
-      #   {
-      #     url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts";
-      #   }
-      # ];
+      lists = piholeLists;
     };
 
     services.pihole-web = {
