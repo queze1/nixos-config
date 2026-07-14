4f4822bc4218f7fc312b6cb50e2459d4d2a7843d
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-14 16:32:19 +1000

    fix: implement sops-nix module

--- modules/lib/sops-nix.nix
+++ modules/lib/sops-nix.nix
@@ -1,5 +1,15 @@
-{
+{inputs, ...}: {
   flake.nixosModules.sops-nix = {
+    imports = [
+      inputs.sops-nix.nixosModules.sops
+    ];
+
+    sops.defaultSopsFile = "${inputs.secrets}/secrets/secrets.yaml";
+    sops.age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+    sops.age.keyFile = "/var/lib/sops-nix/key.txt";
+    sops.age.generateKey = true;
+
+    my.preservation.extraDirectories = ["/var/lib/sops-nix"];
     my.preservation.extraUserDirectories = [".config/sops/age"];
   };
 }
