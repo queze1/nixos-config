4477d02d696e2c2ca697893bab0c5498d70ea333
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-16 16:30:39 +1000

    fix: make ddclient properly get tailscale ip

--- modules/features/hosting/ddns.nix
+++ modules/features/hosting/ddns.nix
@@ -1,13 +1,21 @@
 {
-  flake.nixosModules.ddns = {config, ...}: {
+  flake.nixosModules.ddns = {config, ...}: let
+    tailscale = "${config.services.tailscale.package}/bin/tailscale";
+  in {
     # Dynamically update Cloudflare DNS with Tailscale IP
     services.ddclient = {
       enable = true;
-      usev4 = "ifv4, ifv4=tailscale0";
+      usev4 = ''cmdv4, cmdv4="${tailscale} ip --4"'';
+      usev6 = ''cmdv6, cmdv6="${tailscale} ip --6"'';
       protocol = "cloudflare";
       zone = "osipol.uk";
       passwordFile = config.sops.secrets.osipol-cloudflare-api-token.path;
       username = "token";
+    };
+
+    systemd.services.ddclient = {
+      after = ["tailscaled.service"];
+      wants = ["tailscaled.service"];
     };
   };
 }
