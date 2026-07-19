a6795f0a94a7303e8ade483a925f9c9b10ce17d0
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-19 14:54:20 +1000

    fix: fix iso format

--- modules/iso.nix
+++ modules/iso.nix
@@ -10,20 +10,20 @@
     hostPlatform,
     buildPlatform,
   }:
-    inputs.nixpkgs-stable.lib.nixosSystem
-    {
-      modules = [
-        "${inputs.nixpkgs-stable}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-        self.nixosModules.openssh
-        {
-          # Backdoor the ISO so I can SSH in
-          users.users.root.openssh.authorizedKeys.keys = [sshKeys.ableArcherKey];
-          networking.networkmanager.enable = true;
-          nixpkgs.hostPlatform = hostPlatform;
-          nixpkgs.buildPlatform = buildPlatform;
-        }
-      ];
-    }.config.system.build.isoImage;
+    (inputs.nixpkgs-stable.lib.nixosSystem
+      {
+        modules = [
+          "${inputs.nixpkgs-stable}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+          self.nixosModules.openssh
+          {
+            # Backdoor the ISO so I can SSH in
+            users.users.root.openssh.authorizedKeys.keys = [sshKeys.ableArcherKey];
+            networking.networkmanager.enable = true;
+            nixpkgs.hostPlatform = hostPlatform;
+            nixpkgs.buildPlatform = buildPlatform;
+          }
+        ];
+      }).config.system.build.isoImage;
 in {
   perSystem = {system, ...}: let
   in {
