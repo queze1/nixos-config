9006843dbb081ae09b5c4939e28b1d2fcff55407
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-19 10:44:00 +1000

    chore: set inital pass for root on home servers

--- modules/hosts/steadfast-base/default.nix
+++ modules/hosts/steadfast-base/default.nix
@@ -31,6 +31,9 @@ in {
       pkgs.tree
     ];
 
+    # Remember to lock root password after first use with "passwd -l root"
+    users.users.root.initialPassword = "root";
+
     # Allow SSH into root
     users.users.root.openssh.authorizedKeys.keys = [sshKeys.ableArcherKey];
 
