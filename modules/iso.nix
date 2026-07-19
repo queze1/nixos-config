c9d24dde09ca31024e498fc262aceb6d531dc84e
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-19 18:43:08 +1000

    feat: create shell scripts to burn isos

--- modules/iso.nix
+++ modules/iso.nix
@@ -27,7 +27,9 @@ in {
         ];
       };
 
-    # Helper to generate an ISO
+    # Helper to return
+    # - isoDerivation: a Nix derivation that builds an ISO image
+    # - isoPath: the path to that ISO image
     mkIso = {hostPlatform}: let
       nixos = mkIsoSystem {inherit hostPlatform;};
       isoDerivation = nixos.config.system.build.isoImage;
@@ -35,15 +37,28 @@ in {
       inherit isoDerivation;
       isoPath = "${isoDerivation}/${nixos.config.image.filePath}";
     };
+
+    # Wrapper around xorriso-dd-target to burn an ISO
+    mkIsoBurnerScript = {
+      name,
+      hostPlatform,
+    }:
+      pkgs.writeShellScriptBin name ''
+        ${pkgs.libisoburn}/bin/xorriso-dd-target -with_sudo -plug_test -DO_WRITE -image_file ${(mkIso {inherit hostPlatform;}).isoPath}
+      '';
   in {
     packages = {
       iso-x86 = (mkIso {hostPlatform = "x86_64-linux";}).isoDerivation;
       iso-aarch64 = (mkIso {hostPlatform = "aarch64-linux";}).isoDerivation;
 
-      burn-iso = pkgs.writeShellScriptBin "burn-iso" ''
-        set -e
-        echo ${(mkIso {hostPlatform = "x86_64-linux";}).isoPath}
-      '';
+      burn-iso-x86 = mkIsoBurnerScript {
+        name = "burn-iso-x86";
+        hostPlatform = "x86_64-linux";
+      };
+      burn-iso-aarch64 = mkIsoBurnerScript {
+        name = "burn-iso-aarch64";
+        hostPlatform = "aarch64-linux";
+      };
     };
   };
 }
