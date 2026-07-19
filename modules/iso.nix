71c2b83248acd4383d822cea80e4778bf0068e2a
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-19 14:31:21 +1000

    refactor: simplify process of making iso, don't create new nixosconfigurations

--- modules/iso.nix
+++ modules/iso.nix
@@ -5,9 +5,13 @@
 }: let
   sshKeys = import "${self}/ssh-keys.nix";
 
-  # Helper to create a NixOS system which can generate an ISO for a given platform
-  mkIso = {hostPlatform}:
-    inputs.nixpkgs-stable.lib.nixosSystem {
+  # Helper to make an ISO image
+  mkIso = {
+    hostPlatform,
+    buildPlatform,
+  }:
+    inputs.nixpkgs-stable.lib.nixosSystem
+    {
       modules = [
         "${inputs.nixpkgs-stable}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
         self.nixosModules.openssh
@@ -16,28 +20,22 @@
           users.users.root.openssh.authorizedKeys.keys = [sshKeys.ableArcherKey];
           networking.networkmanager.enable = true;
           nixpkgs.hostPlatform = hostPlatform;
+          nixpkgs.buildPlatform = buildPlatform;
         }
       ];
-    };
+    }.config.system.build.isoImage;
 in {
-  flake.nixosConfigurations = {
-    iso-x86 = mkIso {hostPlatform = "x86_64-linux";};
-    iso-aarch64 = mkIso {hostPlatform = "aarch64-linux";};
-  };
-
   perSystem = {system, ...}: let
-    # Helper which injects the current system as the buildPlatform, then extracts the ISO derivation
-    buildIsoForSystem = nixosConfig:
-      (nixosConfig.extendModules {
-        modules = [{nixpkgs.buildPlatform = system;}];
-      }).config.system.build.isoImage;
   in {
     packages = {
-      # E.g. When you run `nix build .#iso-x86` on an aarch64-linux host,
-      # it evaluates this package under packages.aarch64-linux.iso-x86
-      # and sets buildPlatform = "aarch64-linux"
-      iso-x86 = buildIsoForSystem self.nixosConfigurations.iso-x86;
-      iso-aarch64 = buildIsoForSystem self.nixosConfigurations.iso-aarch64;
+      iso-x86 = mkIso {
+        hostPlatform = "x86_64-linux";
+        buildPlatform = system;
+      };
+      iso-aarch64 = mkIso {
+        hostPlatform = "aarch64-linux";
+        buildPlatform = system;
+      };
     };
   };
 }
