02068ea4029da75752db9ff9274cdcc57a27d40e
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-19 10:21:38 +1000

    docs: add note about why commander user exists

--- modules/users/commander.nix
+++ modules/users/commander.nix
@@ -10,6 +10,7 @@ in {
         "wheel"
       ];
 
+      # Fallback in case internet breaks and you need physical access
       initialHashedPassword = "$y$j9T$nS.BeshWf5D0S1Y0vwHcL/$d3284XHsMtupMdPZDZFtDEyKR/GVEWHkEE.z2T5R4i8";
       openssh.authorizedKeys.keys = [sshKeys.ableArcherKey];
     };
