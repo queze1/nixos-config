bde8cef414a73b56848a364a57faf01edadedef3
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-20 14:39:56 +1000

    fix: don't ban tailscale devices

--- modules/features/services/openssh.nix
+++ modules/features/services/openssh.nix
@@ -7,6 +7,11 @@
         PasswordAuthentication = false;
         PermitRootLogin = "prohibit-password";
       };
+
+      # Don't ban my Tailscale devices
+      extraConfig = ''
+        PerSourcePenaltyExemptList 100.64.0.0/10
+      '';
     };
   };
 }
