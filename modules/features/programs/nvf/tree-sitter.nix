52a07fd426d01472e02798f893ea3cf888d46a38
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-22 10:50:40 +1000

    chore: update treesitter textobjects

--- modules/features/programs/nvf/tree-sitter.nix
+++ modules/features/programs/nvf/tree-sitter.nix
@@ -45,21 +45,30 @@
               require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
             end, { desc = "function [Treesitter]" })
 
-            -- c: class
+            -- c: call
             vim.keymap.set({ "x", "o" }, "ac", function()
+              require("nvim-treesitter-textobjects.select").select_textobject("@call.outer", "textobjects")
+            end, { desc = "call [Treesitter]" })
+
+            vim.keymap.set({ "x", "o" }, "ic", function()
+              require("nvim-treesitter-textobjects.select").select_textobject("@call.inner", "textobjects")
+            end, { desc = "call [Treesitter]" })
+
+            -- C: class
+            vim.keymap.set({ "x", "o" }, "aC", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
             end, { desc = "class [Treesitter]" })
 
-            vim.keymap.set({ "x", "o" }, "ic", function()
+            vim.keymap.set({ "x", "o" }, "iC", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
             end, { desc = "class [Treesitter]" })
 
-            -- m: parameter
-            vim.keymap.set({ "x", "o" }, "am", function()
+            -- a: parameter (a for 'argument')
+            vim.keymap.set({ "x", "o" }, "aa", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@parameter.outer", "textobjects")
             end, { desc = "parameter [Treesitter]" })
 
-            vim.keymap.set({ "x", "o" }, "im", function()
+            vim.keymap.set({ "x", "o" }, "ia", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@parameter.inner", "textobjects")
             end, { desc = "parameter [Treesitter]" })
 
@@ -72,19 +81,31 @@
               require("nvim-treesitter-textobjects.select").select_textobject("@loop.inner", "textobjects")
             end, { desc = "loop [Treesitter]" })
 
-            -- d: conditional
-            vim.keymap.set({ "x", "o" }, "ad", function()
+            -- o: conditional (o for 'or')
+            vim.keymap.set({ "x", "o" }, "ao", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@conditional.outer", "textobjects")
             end, { desc = "conditional [Treesitter]" })
 
-            vim.keymap.set({ "x", "o" }, "id", function()
+            vim.keymap.set({ "x", "o" }, "io", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@conditional.inner", "textobjects")
             end, { desc = "conditional [Treesitter]" })
 
-            -- s: scope
-            vim.keymap.set({ "x", "o" }, "as", function()
-              require("nvim-treesitter-textobjects.select").select_textobject("@local.scope", "locals")
-            end, { desc = "scope [Treesitter]" })
+            -- =: assignment
+            vim.keymap.set({ "x", "o" }, "a=", function()
+              require("nvim-treesitter-textobjects.select").select_textobject("@assignment.outer", "textobjects")
+            end, { desc = "assignment [Treesitter]" })
+
+            vim.keymap.set({ "x", "o" }, "i=", function()
+              require("nvim-treesitter-textobjects.select").select_textobject("@assignment.inner", "textobjects")
+            end, { desc = "assignment [Treesitter]" })
+
+            vim.keymap.set({ "x", "o" }, "l=", function()
+              require("nvim-treesitter-textobjects.select").select_textobject("@assignment.lhs", "textobjects")
+            end, { desc = "assignment LHS [Treesitter]" })
+
+            vim.keymap.set({ "x", "o" }, "r=", function()
+              require("nvim-treesitter-textobjects.select").select_textobject("@assignment.rhs", "textobjects")
+            end, { desc = "assignment RHS [Treesitter]" })
           '';
         };
       };
