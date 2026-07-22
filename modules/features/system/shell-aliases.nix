f80b20c51516396b6e210cffd4deb278db962d58
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-22 17:14:39 +1000

    chore: add nix darwin shell aliases

--- modules/features/system/shell-aliases.nix
+++ modules/features/system/shell-aliases.nix
@@ -4,11 +4,21 @@
     environment.shellAliases = {
       nrs = "sudo --preserve-env=SSH_AUTH_SOCK nixos-rebuild switch --flake ~/etc/nixos#";
       nrb = "sudo --preserve-env=SSH_AUTH_SOCK nixos-rebuild boot --flake ~/etc/nixos#";
+      nfc = "nix flake check";
       nix-direnv-init = "nix flake new -t github:nix-community/nix-direnv .";
     };
 
     environment.systemPackages = [
       self.packages.${pkgs.stdenv.hostPlatform.system}.flake-update
     ];
+  };
+
+  flake.darwinModules.shellAliases = {
+    environment.shellAliases = {
+      nrs = "darwin-rebuild switch --flake ~/.config/nixos#";
+      nrb = "darwin-rebuild build --flake ~/.config/nixos#";
+      nfc = "darwin-rebuild check --flake ~/.config/nixos#";
+      nrr = "darwin-rebuild rollback";
+    };
   };
 }
--- modules/hosts/autumn-forge/default.nix
+++ modules/hosts/autumn-forge/default.nix
@@ -6,17 +6,21 @@
   hostname = "autumn-forge";
 in {
   flake.darwinModules.autumnForgeConfiguration = {pkgs, ...}: {
+    imports = [
+      self.darwinModules.shellAliases
+    ];
+
     environment.systemPackages = [
       pkgs.vim
     ];
 
     programs.fish.enable = true;
 
+    # Enable flakes
     nix.settings.experimental-features = "nix-command flakes";
 
     system.configurationRevision = self.rev or self.dirtyRev or null;
     system.stateVersion = 6;
-
     nixpkgs.hostPlatform = "aarch64-darwin";
   };
 
