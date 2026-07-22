8a4230b4ceb3d3db10014832b756acfc11b5d219
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-22 18:17:53 +1000

    fix: make nix-darwin follow unstable

--- flake.nix
+++ flake.nix
@@ -7,8 +7,8 @@
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-26.05";
     home-manager.url = "github:nix-community/home-manager";
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
-      inputs.nixpkgs.follows = "nixpkgs-stable";
+      url = "github:nix-darwin/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # Secrets
