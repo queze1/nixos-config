f56e64aeb0e86584d8ccd912e7caf354ec5fa6ff
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-22 11:05:35 +1000

    chore: add keybind to go to next/prev arg

--- modules/features/programs/nvf/tree-sitter.nix
+++ modules/features/programs/nvf/tree-sitter.nix
@@ -36,11 +36,12 @@
               },
             })
 
+            -- SELECTION --
+
             -- f: function
             vim.keymap.set({ "x", "o" }, "af", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
             end, { desc = "function [Treesitter]" })
-
             vim.keymap.set({ "x", "o" }, "if", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
             end, { desc = "function [Treesitter]" })
@@ -49,7 +50,6 @@
             vim.keymap.set({ "x", "o" }, "ac", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@call.outer", "textobjects")
             end, { desc = "call [Treesitter]" })
-
             vim.keymap.set({ "x", "o" }, "ic", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@call.inner", "textobjects")
             end, { desc = "call [Treesitter]" })
@@ -58,7 +58,6 @@
             vim.keymap.set({ "x", "o" }, "aC", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
             end, { desc = "class [Treesitter]" })
-
             vim.keymap.set({ "x", "o" }, "iC", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
             end, { desc = "class [Treesitter]" })
@@ -67,7 +66,6 @@
             vim.keymap.set({ "x", "o" }, "aa", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@parameter.outer", "textobjects")
             end, { desc = "parameter [Treesitter]" })
-
             vim.keymap.set({ "x", "o" }, "ia", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@parameter.inner", "textobjects")
             end, { desc = "parameter [Treesitter]" })
@@ -76,7 +74,6 @@
             vim.keymap.set({ "x", "o" }, "al", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@loop.outer", "textobjects")
             end, { desc = "loop [Treesitter]" })
-
             vim.keymap.set({ "x", "o" }, "il", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@loop.inner", "textobjects")
             end, { desc = "loop [Treesitter]" })
@@ -85,7 +82,6 @@
             vim.keymap.set({ "x", "o" }, "ao", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@conditional.outer", "textobjects")
             end, { desc = "conditional [Treesitter]" })
-
             vim.keymap.set({ "x", "o" }, "io", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@conditional.inner", "textobjects")
             end, { desc = "conditional [Treesitter]" })
@@ -94,18 +90,23 @@
             vim.keymap.set({ "x", "o" }, "a=", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@assignment.outer", "textobjects")
             end, { desc = "assignment [Treesitter]" })
-
             vim.keymap.set({ "x", "o" }, "i=", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@assignment.inner", "textobjects")
             end, { desc = "assignment [Treesitter]" })
-
             vim.keymap.set({ "x", "o" }, "l=", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@assignment.lhs", "textobjects")
             end, { desc = "assignment LHS [Treesitter]" })
-
             vim.keymap.set({ "x", "o" }, "r=", function()
               require("nvim-treesitter-textobjects.select").select_textobject("@assignment.rhs", "textobjects")
             end, { desc = "assignment RHS [Treesitter]" })
+
+            -- MOVEMENT --
+            vim.keymap.set({ "x", "o" }, "]a", function()
+              require("nvim-treesitter-textobjects.move").goto_next_start("@parameter.inner", "textobjects")
+            end, { desc = "next parameter [Treesitter]" })
+            vim.keymap.set({ "x", "o" }, "[a", function()
+              require("nvim-treesitter-textobjects.move").goto_previous_start("@parameter.inner", "textobjects")
+            end, { desc = "previous parameter [Treesitter]" })
           '';
         };
       };
