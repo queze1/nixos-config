07d3ae2cce9f4dc9d4745470808de1893615e4fe
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-22 17:04:50 +1000

    chore: remove g shell alias

--- modules/features/system/shell-aliases.nix
+++ modules/features/system/shell-aliases.nix
@@ -5,7 +5,6 @@
       nrs = "sudo --preserve-env=SSH_AUTH_SOCK nixos-rebuild switch --flake ~/etc/nixos#";
       nrb = "sudo --preserve-env=SSH_AUTH_SOCK nixos-rebuild boot --flake ~/etc/nixos#";
       nix-direnv-init = "nix flake new -t github:nix-community/nix-direnv .";
-      g = "git";
     };
 
     environment.systemPackages = [
