2ce8a622c8f6a14f22197f9ee35131dbf830d9f7
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-28 08:49:09 +1000

    fix: give sudoers access to nix-access-tokens

--- modules/features/nix/setup-access-tokens.nix
+++ modules/features/nix/setup-access-tokens.nix
@@ -1,7 +1,10 @@
 {
   flake.nixosModules.setupAccessTokens = {config, ...}: {
-    # Only needed for machines that will be building
-    sops.secrets.nix-access-tokens = {};
+    # Needed to access the secrets GitHub repo
+    sops.secrets.nix-access-tokens = {
+      mode = "0440";
+      group = "wheel"; # give access to sudoers
+    };
     nix.extraOptions = ''
       !include ${config.sops.secrets.nix-access-tokens.path}
     '';
