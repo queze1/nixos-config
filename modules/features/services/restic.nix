89c7b8c94fa5e4e175a9bc11b016f0e3d67ecd95
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-27 10:14:28 +1000

    refactor: remove addDefaults toggle option

--- modules/features/services/restic.nix
+++ modules/features/services/restic.nix
@@ -7,34 +7,23 @@
   }: let
     cfg = config.my.restic;
   in {
-    options.my.restic = {
-      addDefaults = lib.mkOption {
-        type = lib.types.bool;
-        default = true;
-        description = ''
-          Add initialize=True, environmentFile=config.sops.secrets."restic-''${name}-env".path and other defaults to all backups."
-        '';
-      };
-
-      backups = lib.mkOption {
-        type = options.services.restic.backups.type;
-        default = {};
-        description = "Periodic backups to create with Restic.";
-      };
+    options.my.restic.backups = lib.mkOption {
+      type = options.services.restic.backups.type;
+      default = {};
+      description = "Periodic backups to create with Restic.";
     };
 
     config = {
-      sops.secrets = lib.mkIf cfg.addDefaults (
+      sops.secrets =
         lib.mapAttrs' (
           name: _:
             lib.nameValuePair "restic-${name}-env" {}
         )
-        cfg.backups
-      );
+        cfg.backups;
 
       services.restic.backups = lib.mapAttrs (name: backup:
         backup
-        // lib.mkIf cfg.addDefaults {
+        // {
           initialize = lib.mkDefault true;
           environmentFile = lib.mkDefault config.sops.secrets."restic-${name}-env".path;
           timerConfig = {
