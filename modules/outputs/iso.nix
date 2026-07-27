c28a0e0dc538b3f63a26870a1ef4693706a08840
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-27 17:11:28 +1000

    fix: remove cross-compilation for isos to improve performance

--- modules/outputs/iso.nix
+++ modules/outputs/iso.nix
@@ -1,3 +1,5 @@
+# Old implementation: https://github.com/queze1/nixos-config/blob/4401f760011a703e07107047b5a278e73df2d1d4/modules/outputs/iso.nix
+# Previously allowed cross-compilation, removed as it was causing too much lag from nixd and nix flake check
 {
   self,
   inputs,
@@ -6,12 +8,13 @@
   sshKeys = import "${self}/ssh-keys.nix";
 in {
   perSystem = {
+    lib,
     pkgs,
     system,
     ...
   }: let
-    # Helper to generate a NixOS system to build an ISO image of
-    mkIsoSystem = {hostPlatform}:
+    # Define an ISO as a NixOS system
+    isoSystem =
       inputs.nixpkgs-stable.lib.nixosSystem
       {
         modules = [
@@ -21,32 +24,20 @@ in {
             # Backdoor the ISO so I can SSH in
             users.users.root.openssh.authorizedKeys.keys = [sshKeys.ableArcherKey];
             networking.networkmanager.enable = true;
-            nixpkgs.hostPlatform = hostPlatform;
-            nixpkgs.buildPlatform = system;
+            nixpkgs.hostPlatform = system;
           }
         ];
       };
 
-    # Helper to return
-    # - isoDerivation: a Nix derivation that builds an ISO image
-    # - isoPath: the path to that ISO image
-    mkIso = {hostPlatform}: let
-      nixos = mkIsoSystem {inherit hostPlatform;};
-      isoDerivation = nixos.config.system.build.isoImage;
-    in {
-      inherit isoDerivation;
-      isoPath = "${isoDerivation}/${nixos.config.image.filePath}";
-    };
+    isoDerivation = isoSystem.config.system.build.isoImage;
+    isoPath = "${isoDerivation}/${isoSystem.config.image.filePath}";
 
     # Wrapper around xorriso-dd-target to burn an ISO
-    mkIsoBurnerScript = {
-      name,
-      hostPlatform,
-    }: let
+    mkIsoBurnerScript = name: let
       innerScript = pkgs.writeShellScript "burn-iso-internal" ''
         exec xorriso-dd-target \
           -with_sudo -plug_test -DO_WRITE \
-          -image_file ${(mkIso {inherit hostPlatform;}).isoPath} \
+          -image_file ${isoPath} \
           "$@"
       '';
     in
@@ -65,18 +56,9 @@ in {
         runScript = "${innerScript}";
       };
   in {
-    packages = {
-      iso-x86 = (mkIso {hostPlatform = "x86_64-linux";}).isoDerivation;
-      iso-aarch64 = (mkIso {hostPlatform = "aarch64-linux";}).isoDerivation;
-
-      burn-iso-x86 = mkIsoBurnerScript {
-        name = "burn-iso-x86";
-        hostPlatform = "x86_64-linux";
-      };
-      burn-iso-aarch64 = mkIsoBurnerScript {
-        name = "burn-iso-aarch64";
-        hostPlatform = "aarch64-linux";
-      };
+    packages = lib.optionalAttrs pkgs.stdenv.isLinux {
+      iso = isoDerivation;
+      burn-iso = mkIsoBurnerScript "burn-iso";
     };
   };
 }
