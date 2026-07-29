e32e165fda49e3dd8e6ab1083649e67deb972b06
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-29 13:06:35 +1000

    fix: specify package for garage

--- modules/features/hosting/garage.nix
+++ modules/features/hosting/garage.nix
@@ -1,9 +1,14 @@
 {
-  flake.nixosModules.garage = {config, ...}: let
+  flake.nixosModules.garage = {
+    config,
+    pkgs,
+    ...
+  }: let
     cfg = config.services.garage;
   in {
     services.garage = {
       enable = true;
+      package = pkgs.garage_2;
       environmentFile = config.sops.secrets.garage-env.path;
       settings = {
         metadata_dir = "/var/lib/garage/meta";
