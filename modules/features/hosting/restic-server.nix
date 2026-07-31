b99fab84fb297067c8897fbe083c24cc7c4f2fbd
queze1 <52340127+queze1@users.noreply.github.com>
2026-07-31 11:27:36 +1000

    feat: implement restic server

---
+++ modules/features/hosting/restic-server.nix
@@ -0,0 +1,41 @@
+{
+  flake.nixosModules.resticServer = {config, ...}: let
+    cfg = config.restic.server;
+    socketPath = "/run/restic/restic.sock";
+  in {
+    services.restic.server = {
+      enable = true;
+      appendOnly = true;
+      listenAddress = "unix:${socketPath}";
+      privateRepos = true;
+    };
+
+    # Preserve restic repository
+    my.preservation.extraDirectories = [
+      {
+        directory = cfg.dataDir;
+        user = cfg.user;
+        group = cfg.group;
+        mode = "0700";
+      }
+    ];
+
+    # Back up restic repository
+    my.restic.extraPaths = [("/persistent" + cfg.dataDir)];
+
+    # Give Caddy access to the socket
+    users.users.${config.services.caddy.user}.extraGroups = [cfg.group];
+
+    # Reverse proxy with Tailscale auth
+    services.caddy.virtualHosts = {
+      "restic-server.osipol.uk" = {
+        extraConfig = ''
+          import cloudflare_dns
+          import tailscale_auth
+          reverse_proxy unix/${socketPath}
+        '';
+      };
+    };
+    services.ddclient.domains = ["restic-server.osipol.uk"];
+  };
+}
