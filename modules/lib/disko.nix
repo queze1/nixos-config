2c5de8c7bb165aaa124347691939c6ae6f4bd091
queze1 <52340127+queze1@users.noreply.github.com>
2026-08-05 11:03:58 +1000

    fix: comment out specialisations

--- modules/lib/disko.nix
+++ modules/lib/disko.nix
@@ -96,12 +96,12 @@
       umount-top-level = "sudo umount /mnt/top-level";
     };
 
-    specialisation.persistent-restore.configuration = {lib, ...}: {
-      fileSystems."/persistent".options = lib.mkForce [
-        "subvol=persistent-restore"
-        "noatime"
-      ];
-    };
+    # specialisation.persistent-restore.configuration = {lib, ...}: {
+    #   fileSystems."/persistent".options = lib.mkForce [
+    #     "subvol=persistent-restore"
+    #     "noatime"
+    #   ];
+    # };
 
     disko.devices.nodev = {
       "/" = {
@@ -272,19 +272,19 @@
       umount-top-level = "sudo umount /mnt/top-level";
     };
 
-    specialisation.root-restore.configuration = {lib, ...}: {
-      fileSystems."/root".options = lib.mkForce [
-        "subvol=root-restore"
-        "noatime"
-      ];
-    };
+    # specialisation.root-restore.configuration = {lib, ...}: {
+    #   fileSystems."/root".options = lib.mkForce [
+    #     "subvol=root-restore"
+    #     "noatime"
+    #   ];
+    # };
 
-    specialisation.persistent-restore.configuration = {lib, ...}: {
-      fileSystems."/persistent".options = lib.mkForce [
-        "subvol=persistent-restore"
-        "noatime"
-      ];
-    };
+    # specialisation.persistent-restore.configuration = {lib, ...}: {
+    #   fileSystems."/persistent".options = lib.mkForce [
+    #     "subvol=persistent-restore"
+    #     "noatime"
+    #   ];
+    # };
 
     disko.devices.disk.main = {
       device = device;
