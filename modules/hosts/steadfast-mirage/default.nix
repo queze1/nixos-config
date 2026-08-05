e439393141b35d505abfac1362934cd943eaba5f
queze1 <52340127+queze1@users.noreply.github.com>
2026-08-05 16:12:13 +1000

    feat: create steadfast-mirage for testing

---
+++ modules/hosts/steadfast-mirage/default.nix
@@ -0,0 +1,71 @@
+{
+  inputs,
+  self,
+  ...
+}: let
+  hostname = "steadfast-mirage";
+in {
+  # Configuration for testing on cloud
+  flake.nixosModules.steadfastMirageConfiguration = {
+    imports = [
+      self.nixosModules.steadfastBase
+
+      self.nixosModules.btrbk
+      (self.factory.diskoBrtfsEphemeralRoot
+        {device = "/dev/vda";})
+      self.nixosModules.podmanContainers
+
+      # Ingress & routing
+      self.nixosModules.caddy
+      self.nixosModules.cloudflared
+      self.nixosModules.ddns
+      self.nixosModules.tailscaleAuth
+
+      # Self-hosted apps
+      self.nixosModules.actual
+      self.nixosModules.arkRpVisualisation
+      self.nixosModules.musicStack
+      self.nixosModules.pihole
+      self.nixosModules.sillytavern
+    ];
+
+    # Set incrementing port numbers
+    services.actual.settings.port = 8000;
+    services.ark-rp-viz.port = 8001;
+    services.metube.port = 8002;
+    services.picard.port = 8003;
+    services.pihole-web.ports = ["8004"];
+    services.sillytavern.port = 8005;
+    services.yubal.port = 8006;
+
+    my.restic = {
+      snapshotsDir = "/persistent/snapshots";
+      extraPaths = [
+        "/etc/ssh"
+        "/var/lib/nixos"
+        "/var/lib/tailscale"
+      ];
+      backups.backblaze-b2 = {
+        timerConfig = {
+          OnCalendar = "daily";
+          RandomizedDelaySec = "4h";
+          Persistent = true;
+        };
+      };
+      backups.local-server = {
+        timerConfig = {
+          OnCalendar = "hourly";
+          RandomizedDelaySec = "15m";
+          Persistent = true;
+        };
+      };
+    };
+
+    hardware.facter.reportPath = ./facter.json;
+    networking.hostName = hostname;
+  };
+
+  flake.nixosConfigurations.${hostname} = inputs.nixpkgs-stable.lib.nixosSystem {
+    modules = [self.nixosModules.steadfastMirageConfiguration];
+  };
+}
