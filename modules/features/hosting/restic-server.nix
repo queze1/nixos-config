9fc34781c6079dd1f7d9584f358623b9a1074ca5
queze1 <52340127+queze1@users.noreply.github.com>
2026-08-06 19:09:25 +1000

    refactor: make restic server use custom option

--- modules/features/hosting/restic-server.nix
+++ modules/features/hosting/restic-server.nix
@@ -1,45 +1,63 @@
 {
-  flake.nixosModules.resticServer = {config, ...}: let
+  flake.nixosModules.resticServer = {
+    config,
+    lib,
+    ...
+  }: let
     cfg = config.services.restic.server;
-    socketPath = "/run/restic/restic.sock";
-    port = 8443; # use whitelisted port as servers need access
+    myCfg = config.my.apps.resticServer;
+    port = 8443; # host on whitelisted port as servers need access
   in {
-    services.restic.server = {
-      enable = true;
-      listenAddress = socketPath;
-      htpasswd-file = config.sops.secrets.restic-server-htpasswd.path;
-      appendOnly = true;
-      privateRepos = true;
-    };
-
-    sops.secrets.restic-server-htpasswd = {
-      owner = "restic";
-      group = "restic";
-    };
-
-    # Preserve restic repository
-    my.preservation.extraDirectories = [
-      {
-        directory = cfg.dataDir;
-        user = "restic";
-        group = "restic";
-        mode = "0700";
-      }
-    ];
-
-    # Give Caddy access to the socket
-    users.users.${config.services.caddy.user}.extraGroups = ["restic"];
-
-    # Reverse proxy
-    # NOTE: Don't use tailscale_auth for any service which needs to be reachable by a server, it automatically rejects tagged devices
-    services.caddy.virtualHosts = {
-      "restic-server.osipol.uk:${toString port}" = {
-        extraConfig = ''
-          import cloudflare_dns
-          reverse_proxy unix/${socketPath}
-        '';
+    options.my.apps.resticServer = {
+      domain = lib.mkOption {
+        type = lib.types.str;
+        default = "restic-server.osipol.uk";
+        description = "Domain to host rest-server on.";
+      };
+      socketPath = lib.mkOption {
+        type = lib.types.str;
+        default = "/run/restic/restic.sock";
+        description = "Socket to run rest-server on.";
       };
     };
-    services.ddclient.domains = ["restic-server.osipol.uk"];
+    config = {
+      services.restic.server = {
+        enable = true;
+        listenAddress = myCfg.socketPath;
+        htpasswd-file = config.sops.secrets.restic-server-htpasswd.path;
+        appendOnly = true;
+        privateRepos = true;
+      };
+
+      sops.secrets.restic-server-htpasswd = {
+        owner = "restic";
+        group = "restic";
+      };
+
+      # Preserve restic repository
+      my.preservation.extraDirectories = [
+        {
+          directory = cfg.dataDir;
+          user = "restic";
+          group = "restic";
+          mode = "0700";
+        }
+      ];
+
+      # Give Caddy access to the socket
+      users.users.${config.services.caddy.user}.extraGroups = ["restic"];
+
+      # Reverse proxy
+      # NOTE: Don't use tailscale_auth for any service which needs to be reachable by a server, it automatically rejects tagged devices
+      services.caddy.virtualHosts = {
+        "${myCfg.domain}:${toString port}" = {
+          extraConfig = ''
+            import cloudflare_dns
+            reverse_proxy unix/${myCfg.socketPath}
+          '';
+        };
+      };
+      services.ddclient.domains = [myCfg.domain];
+    };
   };
 }
