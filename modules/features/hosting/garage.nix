449e5e31ce9fa1b76483c5b7cf7d92190b8c1792
queze1 <52340127+queze1@users.noreply.github.com>
2026-08-10 10:46:17 +1000

    fix: make garage use tailscale

--- modules/features/hosting/garage.nix
+++ modules/features/hosting/garage.nix
@@ -1,5 +1,3 @@
-# CURRENTLY UNUSED, use if you want a NAS and not just Restic backup
-# CHANGE TO USE TAILSCALE INSTEAD
 {
   flake.nixosModules.garage = {
     config,
@@ -7,76 +5,102 @@
     pkgs,
     ...
   }: let
-    apiPort = 3900;
+    myCfg = config.my.garage;
   in {
-    services.garage = {
-      enable = true;
-      package = pkgs.garage_2;
-      environmentFile = config.sops.secrets.garage-env.path;
-      settings = {
-        metadata_dir = "/var/lib/garage/meta";
-        data_dir = "/var/lib/garage/data";
-
-        db_engine = "sqlite";
-        replication_factor = 1;
-
-        rpc_bind_addr = "[::]:3901";
-        rpc_public_addr = "127.0.0.1:3901";
-
-        s3_api = {
-          s3_region = "garage";
-          api_bind_addr = "[::]:${toString apiPort}";
-          root_domain = ".garage-s3.osipol.uk";
-        };
-
-        admin = {
-          api_bind_addr = "[::]:3903";
-        };
+    options.my.garage = {
+      domain = lib.mkOption {
+        type = lib.types.str;
+        default = "garage.osipol.uk";
+        description = "Domain to host Garage on.";
+      };
+      dataDir = lib.mkOption {
+        type = lib.types.str;
+        default = "/var/lib/garage";
+        description = "Path where Garage stores its data.";
+      };
+      apiSocketPath = lib.mkOption {
+        type = lib.types.str;
+        default = "/run/garage/s3_api.sock";
+        description = "Socket where Garage serves its S3 API.";
+      };
+      adminSocketPath = lib.mkOption {
+        type = lib.types.str;
+        default = "/run/garage/admin.sock";
+        description = "Socket where Garage serves its S3 API.";
       };
     };
 
-    sops.secrets.garage-env = {
-      restartUnits = ["garage.service"];
-    };
+    config = {
+      services.garage = {
+        enable = true;
+        package = pkgs.garage_2;
+        environmentFile = config.sops.secrets.garage-env.path;
+        settings = {
+          metadata_dir = "${myCfg.dataDir}/meta";
+          data_dir = "${myCfg.dataDir}/data";
 
-    # Create a system user to run Garage
-    users.users.garage = {
-      isSystemUser = true;
-      group = "garage";
-    };
-    users.groups.garage = {};
+          db_engine = "sqlite";
+          replication_factor = 1;
 
-    systemd.services.garage = {
-      serviceConfig = {
-        DynamicUser = lib.mkForce false;
-        User = "garage";
-        Group = "garage";
-      };
-    };
+          rpc_bind_addr = "127.0.0.1:3901";
+          rpc_public_addr = "127.0.0.1:3901";
 
-    # Preserve Garage data
-    my.preservation.extraDirectories = [
-      {
-        directory = "/var/lib/garage";
-        user = "garage";
-        group = "garage";
-        mode = "700";
-      }
-    ];
+          s3_api = {
+            s3_region = "garage";
+            api_bind_addr = myCfg.apiSocketPath;
+            root_domain = ".${myCfg.domain}";
+          };
 
-    # Networking with Cloudflare tunnel
-    services.cloudflared = {
-      enable = true;
-      tunnels = {
-        "9d3af70e-bb75-4731-a99c-145865a1bb5f" = {
-          credentialsFile = "${config.sops.secrets.garage-cloudflare-creds.path}";
-          default = "http_status:404";
-          ingress = {
-            "garage-s3.osipol.uk" = "http://127.0.0.1:${toString apiPort}";
+          admin = {
+            api_bind_addr = myCfg.adminSocketPath;
           };
         };
       };
+
+      sops.secrets.garage-env = {
+        restartUnits = ["garage.service"];
+      };
+
+      # Create a system user to run Garage
+      users.users.garage = {
+        isSystemUser = true;
+        group = "garage";
+      };
+      users.groups.garage = {};
+
+      systemd.services.garage = {
+        serviceConfig = {
+          DynamicUser = lib.mkForce false;
+          User = "garage";
+          Group = "garage";
+        };
+      };
+
+      # Give Caddy access to sockets
+      users.users.${config.services.caddy.user}.extraGroups = ["garage"];
+
+      # Preserve Garage data
+      my.preservation.extraDirectories = [
+        {
+          directory = myCfg.dataDir;
+          user = "garage";
+          group = "garage";
+          mode = "700";
+        }
+      ];
+
+      # Reverse proxy
+      services.caddy.virtualHosts = {
+        "s3.${myCfg.domain}, *.s3.${myCfg.domain}".extraConfig = ''
+          import cloudflare_dns
+          reverse_proxy unix/${myCfg.apiSocketPath}
+        '';
+        "admin.${myCfg.domain}".extraConfig = ''
+          import cloudflare_dns
+          reverse_proxy unix/${myCfg.adminSocketPath}
+        '';
+      };
+      services.ddclient.domains = ["*.${myCfg.domain}"];
     };
-    sops.secrets.garage-cloudflare-creds = {};
   };
 }
