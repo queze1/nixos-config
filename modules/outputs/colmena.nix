b3185967e7ab18e8bb64e442da62bd5367c58103
queze1 <52340127+queze1@users.noreply.github.com>
2026-08-16 14:43:16 +1000

    fix: fix colmena architectures

--- modules/outputs/colmena.nix
+++ modules/outputs/colmena.nix
@@ -8,37 +8,25 @@
       system = "x86_64-linux";
       config.allowUnfree = true;
     };
-    pkgs_aarch64 = import inputs.nixpkgs-stable {
-      system = "aarch64-linux";
-      config.allowUnfree = true;
-    };
   in
     inputs.colmena.lib.makeHive {
       meta = {
         nixpkgs = pkgs_x86;
-        nodeNixpkgs = {
-          mirage-white = pkgs_aarch64;
-        };
       };
 
       defaults = {
-        deployment.targetPort = 22;
         deployment.targetUser = "root";
+        deployment.targetPort = 22;
+        deployment.buildOnTarget = true;
       };
 
       steadfast-dart = {config, ...}: {
-        deployment = {
-          buildOnTarget = true;
-          targetHost = config.networking.hostName;
-        };
+        deployment.targetHost = config.networking.hostName;
         imports = [self.nixosModules.steadfastDartConfiguration];
       };
 
       steadfast-defender = {config, ...}: {
-        deployment = {
-          buildOnTarget = true;
-          targetHost = config.networking.hostName;
-        };
+        deployment.targetHost = config.networking.hostName;
         imports = [self.nixosModules.steadfastDefenderConfiguration];
       };
 
@@ -47,11 +35,7 @@
       # };
 
       mirage-white = {
-        deployment = {
-          buildOnTarget = true;
-          targetHost = "170.64.131.90";
-        };
-
+        deployment.targetHost = "170.64.131.90";
         imports = [
           self.nixosModules.mirageWhiteConfiguration
           self.nixosModules.mirageWhiteHardware
