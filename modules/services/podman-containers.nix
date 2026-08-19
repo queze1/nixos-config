e82b2d75d29206c1eaf2988f4cd6da04e0f4ec5d
queze1 <52340127+queze1@users.noreply.github.com>
2026-08-20 07:41:58 +1000

    chore: don't preserve container storage

--- modules/services/podman-containers.nix
+++ modules/services/podman-containers.nix
@@ -16,15 +16,5 @@ in {
         dockerCompat = true;
       };
     };
-
-    # Preserve container storage so images aren't stored in RAM
-    my.preservation.extraDirectories = [
-      {
-        directory = "/var/lib/containers";
-        user = "root";
-        group = "root";
-        mode = "0700";
-      }
-    ];
   };
 }
