655a7ebfc59836035b3b09dcc9473e5d54e36363
queze1 <52340127+queze1@users.noreply.github.com>
2026-08-23 16:59:08 +1000

    chore: add extra hardening to cloudflare tunnel

--- modules/hosting/infra/cloudflared.nix
+++ modules/hosting/infra/cloudflared.nix
@@ -7,5 +7,35 @@
 
   config = lib.mkIf config.my.cloudflared.enable {
     services.cloudflared.enable = true;
+
+    # Hardening
+    systemd.services =
+      lib.mapAttrs' (
+        name: _:
+          lib.nameValuePair "cloudflared-tunnel-${name}" {
+            serviceConfig = {
+              NoNewPrivileges = true;
+              PrivateDevices = true;
+              PrivateTmp = true;
+              ProtectClock = true;
+              ProtectControlGroups = true;
+              ProtectHome = true;
+              ProtectHostname = true;
+              ProtectKernelLogs = true;
+              ProtectKernelModules = true;
+              ProtectKernelTunables = true;
+              ProtectSystem = "strict";
+              RestrictAddressFamilies = ["AF_INET" "AF_INET6" "AF_UNIX" "AF_NETLINK"];
+              RestrictNamespaces = true;
+              RestrictRealtime = true;
+              RestrictSUIDSGID = true;
+              LockPersonality = true;
+              SystemCallArchitectures = "native";
+              SystemCallFilter = ["@system-service" "~@privileged @resources"];
+              UMask = "0077";
+            };
+          }
+      )
+      config.services.cloudflared.tunnels;
   };
 }
