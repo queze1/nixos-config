5816fa2ebdabddcec3348328243783635122f81f
queze1 <52340127+queze1@users.noreply.github.com>
2026-08-25 14:54:53 +1000

    build: make comin require nix access tokens, and do a faster poll

--- modules/deployment/comin.nix
+++ modules/deployment/comin.nix
@@ -11,6 +11,13 @@ in {
   options.my.deployment.comin.enable = lib.mkEnableOption "Comin";
 
   config = lib.mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = config.my.nix.accessTokens.enable;
+        message = "comin requires Nix access tokens";
+      }
+    ];
+
     services.comin = {
       enable = true;
       remotes = [
@@ -18,6 +25,8 @@ in {
           name = "origin";
           url = "https://github.com/queze1/nixos-config.git";
           branches.main.name = "deployed";
+          auth.access_token_path = config.sops.secrets.nix-access-tokens.path;
+          poller.period = 15;
         }
       ];
     };
