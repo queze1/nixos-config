959ccd3ae85d3396fb3916c8e593a607fe35251f
queze1 <52340127+queze1@users.noreply.github.com>
2026-08-25 13:59:50 +1000

    chore: package filebrowser quantum

---
+++ outputs/packages.nix
@@ -0,0 +1,37 @@
+{inputs, ...}: {
+  perSystem = {pkgs, ...}: {
+    packages.filebrowser-quantum = pkgs.buildGoModule (let
+      version = "unstable-2026-08-25";
+      frontend = pkgs.buildNpmPackage {
+        pname = "filebrowser-quantum-frontend";
+        inherit version;
+        src = inputs.filebrowser-quantum;
+        sourceRoot = "source/frontend";
+        npmDepsHash = "sha256-9UMr7Lm2Z0NSmeFdYEoeUo0/ASzWGke5swAOyyecV30=";
+        postPatch = ''
+          chmod -R u+w ../backend
+        '';
+        installPhase = ''
+          install -d $out
+          cp -r ../backend/internal/web/embed/. $out
+        '';
+      };
+    in {
+      pname = "filebrowser-quantum";
+      inherit version;
+      src = inputs.filebrowser-quantum;
+      sourceRoot = "source/backend";
+      vendorHash = "sha256-d0YY7FovQeMlxoNL1wz2pSiWeGd3l05L6MOSuX0FT4U=";
+      subPackages = ["."];
+      doCheck = false;
+
+      preBuild = ''
+        cp -r ${frontend}/. internal/web/embed
+      '';
+
+      postInstall = ''
+        mv $out/bin/backend $out/bin/filebrowser-quantum
+      '';
+    });
+  };
+}
