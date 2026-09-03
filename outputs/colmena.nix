3f3627c23b0fc89ce3dff2ad96e09e1c50526ae3
queze1 <52340127+queze1@users.noreply.github.com>
2026-09-03 16:32:52 +1000

    fix: make colmena explicitly use the tailscale domain for all hosts

--- outputs/colmena.nix
+++ outputs/colmena.nix
@@ -20,34 +20,33 @@
         };
       };
 
-      defaults = {
+      defaults = {config, ...}: {
         imports = [commonModules];
 
         deployment = {
           targetUser = "root";
           targetPort = 22;
+          targetHost = "${config.networking.hostName}.${config.my.constants.tailnetDomain}"; # deploy over Tailscale
           buildOnTarget = true;
         };
       };
 
-      steadfast-dart = {config, ...}: {
-        deployment.targetHost = config.networking.hostName;
+      steadfast-dart = {
         deployment.tags = ["local"];
         imports = [{my.hosts.steadfast-dart.enable = true;}];
       };
 
-      steadfast-defender = {config, ...}: {
-        deployment.targetHost = config.networking.hostName;
+      steadfast-defender = {
         deployment.tags = ["local"];
         imports = [{my.hosts.steadfast-defender.enable = true;}];
       };
 
       # steadfast-noon = {
+      #   deployment.tags = ["local"];
       #   imports = [self.nixosModules.steadfastNoonConfiguration];
       # };
 
       # mirage-white = {
-      #   deployment.targetHost = "170.64.131.90";
       #   deployment.tags = ["cloud"];
       #   imports = [
       #     self.nixosModules.mirageWhiteConfiguration
@@ -56,7 +55,6 @@
       # };
 
       mirage-red = {
-        deployment.targetHost = "192.9.184.187";
         deployment.tags = ["cloud"];
         imports = [
           {my.hosts.mirage-red.enable = true;}
@@ -65,7 +63,6 @@
       };
 
       mirage-blue = {
-        deployment.targetHost = "137.23.9.31";
         deployment.tags = ["cloud"];
         imports = [
           {my.hosts.mirage-blue.enable = true;}
