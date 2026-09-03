520d6b5b2a88bcc00e015beb5551b3b740f73767
queze1 <52340127+queze1@users.noreply.github.com>
2026-09-03 17:30:07 +1000

    chore: make flake-update use nix specified git

--- outputs/shell-scripts.nix
+++ outputs/shell-scripts.nix
@@ -8,35 +8,38 @@
   in {
     packages = lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
       # flake-update: Update and commit NixOS config flake
-      flake-update = pkgs.writeShellScriptBin "flake-update" ''
-        set -e
-        cd ~/etc/nixos
+      flake-update = let
+        git = lib.getExe pkgs.git;
+      in
+        pkgs.writeShellScriptBin "flake-update" ''
+          set -e
+          cd ~/etc/nixos
 
-        # Stash changes
-        echo "Stashing changes..."
-        STASHED=$(git stash push -m "pre-update-automated-stash" --include-untracked)
+          # Stash changes
+          echo "Stashing changes..."
+          STASHED=$(${git} stash push -m "pre-update-automated-stash" --include-untracked)
 
-        # Update flake inputs
-        echo "Updating flake input(s): $@"
-        nix flake update "$@"
+          # Update flake inputs
+          echo "Updating flake input(s): $@"
+          nix flake update "$@"
 
-        # Commit and push the lock file
-        if ! git diff --quiet flake.lock; then
-          echo "Committing lockfile..."
-          git add flake.lock
-          git commit -m "chore: update flake ($*)" -- flake.lock
-          echo "Pushing changes..."
-          git push
-        else
-          echo "flake.lock is already up to date. Skipping commit."
-        fi
+          # Commit and push the lock file
+          if ! ${git} diff --quiet flake.lock; then
+            echo "Committing lockfile..."
+            ${git} add flake.lock
+            ${git} commit -m "chore: update flake ($*)" -- flake.lock
+            echo "Pushing changes..."
+            ${git} push
+          else
+            echo "flake.lock is already up to date. Skipping commit."
+          fi
 
-        # Unstash changes
-        if [[ "$STASHED" != "No local changes to save" ]]; then
-          echo "Restoring stashed changes..."
-          git stash pop || echo "Stash pop resulted in conflicts. Please resolve manually."
-        fi
-      '';
+          # Unstash changes
+          if [[ "$STASHED" != "No local changes to save" ]]; then
+            echo "Restoring stashed changes..."
+            ${git} stash pop || echo "Stash pop resulted in conflicts. Please resolve manually."
+          fi
+        '';
 
       # install: Bootstrap a machine with nixos-anywhere
       install = pkgs.writeShellApplication {
