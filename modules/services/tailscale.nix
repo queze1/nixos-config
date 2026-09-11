e317d7f114de5aa26bd449d0309d43dc247fcf24
queze1 <52340127+queze1@users.noreply.github.com>
2026-09-11 11:47:09 +1000

    feat: add options for exit nodes for tailscale

--- modules/services/tailscale.nix
+++ modules/services/tailscale.nix
@@ -15,14 +15,34 @@ in {
     enable = lib.mkEnableOption "Tailscale";
     useAuthKey = lib.mkEnableOption "using an auth key";
     setHostname = lib.mkEnableOption "explicitly setting the Tailscale hostname to the hostname defined in Nix";
-    openSSHOnlyOnTailscale = lib.mkEnableOption "allowing OpenSSH access only through the Tailscale interface";
+    openSSHOnlyOnTailscale = lib.mkEnableOption "opening OpenSSH ports only on the Tailscale interface";
+    exitNode = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      description = ''
+        The IP address of the exit node to use. When null, no exit node is used.
+      '';
+    };
+    advertiseExitNode = lib.mkEnableOption "advertising this device as an exit node";
   };
 
   config = lib.mkIf cfg.enable {
     services.tailscale = {
       enable = true;
       authKeyFile = lib.mkIf cfg.useAuthKey config.sops.secrets.tailscale-auth-key.path;
-      extraUpFlags = lib.mkIf cfg.setHostname ["--hostname=${config.networking.hostName}"];
+      useRoutingFeatures =
+        if cfg.useExitNode && cfg.advertiseExitNode
+        then "both"
+        else if cfg.useExitNode
+        then "client"
+        else if cfg.advertiseExitNode
+        then "server"
+        else "none";
+      extraUpFlags =
+        []
+        ++ lib.optional cfg.setHostname "--hostname=${config.networking.hostName}"
+        ++ lib.optional cfg.exitNode "--exit-node=${cfg.exitNode}"
+        ++ lib.optional cfg.advertiseExitNode "--advertise-exit-node";
     };
 
     sops.secrets = lib.mkIf cfg.useAuthKey {
