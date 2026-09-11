67f938cfc134ccb1263900008f8582d55e7cd61e
queze1 <52340127+queze1@users.noreply.github.com>
2026-09-11 12:21:19 +1000

    fix: force check reverse path in protonvpn

--- modules/programs/protonvpn.nix
+++ modules/programs/protonvpn.nix
@@ -9,7 +9,7 @@ in {
   options.my.programs.protonvpn.enable = lib.mkEnableOption "Proton VPN" // {default = config.my.programs.enableAll;};
 
   config = lib.mkIf cfg.enable {
-    networking.firewall.checkReversePath = false;
+    networking.firewall.checkReversePath = lib.mkForce false;
     environment.systemPackages = [pkgs.proton-vpn];
   };
 }
