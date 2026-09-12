8cd99cf0478302320249db1680626043715ef3be
queze1 <52340127+queze1@users.noreply.github.com>
2026-09-12 15:47:28 +1000

    feat: create a wip iron hammer host

---
+++ modules/hosts/iron-hammer.nix
@@ -0,0 +1,81 @@
+{
+  config,
+  lib,
+  pkgs-stable,
+  ...
+}: let
+  cfg = config.my.hosts.iron-hammer;
+  mainUser = "queze";
+in {
+  options.my.hosts.iron-hammer.enable =
+    lib.mkEnableOption "iron-hammer host configuration";
+
+  config = lib.mkIf cfg.enable {
+    # System config
+    my.boot = {
+      systemdBoot.enable = true;
+      useLatestLtsKernel = true;
+      configurationLimit = 10;
+    };
+    my.sound.enable = true;
+    my.fonts.enable = true;
+    my.localisation.enable = true;
+    my.networkManager.enable = true;
+    zramSwap.enable = true;
+
+    # Desktop environment
+    my.desktop = {
+      enable = true;
+      niri.enable = true;
+      noctalia.enable = true;
+    };
+
+    # Disk configuration
+    my.disko.btrfsEphemeralRoot.device = "/dev/sda"; # TODO: check what the device name actually is
+    my.preservation = {
+      enable = true;
+      users = [mainUser];
+    };
+    my.btrbk.enable = true;
+
+    # Secret management
+    my.sops = {
+      enable = true;
+      homeManager.enable = true;
+    };
+
+    # Services
+    my.tailscale.enable = true;
+
+    # User management
+    my.homeManager = {
+      enable = true;
+      pkgsStable = pkgs-stable;
+    };
+    my.programs.enableAll = true;
+    my.users.${mainUser}.enable = true;
+
+    # Personalisation
+    my.shortcuts.enable = true;
+    my.editor.vim.enable = true;
+
+    # Nix-related config
+    my.nix = {
+      enable = true;
+      settings.download-buffer-size = 5000000;
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 7d";
+      };
+      binaryCache.enable = true;
+      replHistory.enable = true;
+      accessTokens.enable = true;
+    };
+
+    # TODO: Add backups
+
+    networking.hostName = "iron-hammer";
+    system.stateVersion = "26.11";
+  };
+}
--- outputs/hosts/iron-hammer.nix
+++ outputs/hosts/iron-hammer.nix
@@ -7,7 +7,7 @@
 in {
   flake.nixosConfigurations.${hostname} = self.factory.mkNixosSystem {
     nixpkgs = inputs.nixpkgs;
-    system = "aarch64-linux";
+    system = "x86_64-linux";
     modules = [{my.hosts.${hostname}.enable = true;}];
     extraPkgs.pkgs-stable = inputs.nixpkgs-stable;
   };
