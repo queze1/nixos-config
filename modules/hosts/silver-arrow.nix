36f5b8121a0be43636bd192bf34a887af244b6dd
queze1 <52340127+queze1@users.noreply.github.com>
2026-09-12 15:18:37 +1000

    fix: fix typo in darwin rebuild name

--- modules/hosts/silver-arrow.nix
+++ modules/hosts/silver-arrow.nix
@@ -12,7 +12,7 @@ in {
 
   config = lib.mkIf cfg.enable {
     environment.shellAliases = let
-      darwinRebuild = "sudo darwin-rebuild --refresh --flake github1:queze/nixos-config#${hostname}";
+      darwinRebuild = "sudo darwin-rebuild --refresh --flake github:queze1/nixos-config#${hostname}";
     in {
       nrs = "${darwinRebuild} switch";
       nrb = "${darwinRebuild} boot";
