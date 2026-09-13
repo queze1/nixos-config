0907ee84b60ea79a212dcd5a84c4d8ca7631f8f2
queze1 <52340127+queze1@users.noreply.github.com>
2026-09-13 14:45:34 +1000

    chore: enable binary cache for iron hammer

--- modules/hosts/iron-hammer.nix
+++ modules/hosts/iron-hammer.nix
@@ -73,7 +73,7 @@ in {
         dates = "weekly";
         options = "--delete-older-than 7d";
       };
-      # binaryCache.enable = true;
+      binaryCache.enable = true;
       replHistory.enable = true;
       accessTokens.enable = true;
     };
