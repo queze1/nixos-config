8ad576e6b399b94c5638241df695a98b52f72654
queze1 <52340127+queze1@users.noreply.github.com>
2026-09-15 01:13:39 +1000

    feat: add backups to iron hammer

--- modules/hosts/iron-hammer.nix
+++ modules/hosts/iron-hammer.nix
@@ -44,6 +44,33 @@ in {
       yazi.enable = true;
     };
 
+    # Backups
+    my.restic = {
+      enable = true;
+      snapshotsDir = "/persistent/snapshots";
+      backups.backblaze-b2 = {
+        timerConfig = {
+          OnCalendar = "daily";
+          RandomizedDelaySec = "4h";
+          Persistent = true;
+        };
+      };
+      backups.local-server = {
+        timerConfig = {
+          OnCalendar = "hourly";
+          RandomizedDelaySec = "15m";
+          Persistent = true;
+        };
+      };
+      backups.local-server2 = {
+        timerConfig = {
+          OnCalendar = "hourly";
+          RandomizedDelaySec = "15m";
+          Persistent = true;
+        };
+      };
+    };
+
     networking.hostName = "iron-hammer";
     hardware.facter.reportPath = "${inputs.secrets}/facter/iron-hammer.json";
     system.stateVersion = "26.11";
