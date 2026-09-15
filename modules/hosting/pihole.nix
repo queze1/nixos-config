fd478413124fd2811d05cdf3a95fffe2dc1cc6fb
queze1 <52340127+queze1@users.noreply.github.com>
2026-09-15 18:44:00 +1000

    chore: enable ipv6 cloudflare dns resolvers

--- modules/hosting/pihole.nix
+++ modules/hosting/pihole.nix
@@ -70,9 +70,8 @@ in {
             # Cloudflare DNS
             "1.1.1.1"
             "1.0.0.1"
-            # Servers don't have IPv6 for some reason
-            # "2606:4700:4700::1111"
-            # "2606:4700:4700::1001"
+            "2606:4700:4700::1111"
+            "2606:4700:4700::1001"
           ];
           listeningMode = "SINGLE";
           interface = config.services.tailscale.interfaceName;
