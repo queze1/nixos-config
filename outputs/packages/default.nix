b0c845b500be37726f5890ec2470a73321bcb7e0
queze1 <52340127+queze1@users.noreply.github.com>
2026-09-16 12:57:40 +1000

    refactor: split out creamlinux

--- outputs/packages/default.nix
+++ outputs/packages/default.nix
@@ -1,12 +1,6 @@
-{inputs, ...}: {
-  perSystem = {
-    config,
-    pkgs,
-    ...
-  }: {
+{
+  perSystem = {config, ...}: {
     # Merge myPackages into packages
     packages = config.myPackages;
-
-    myPackages.creamlinux-installer = import inputs.creamlinux-installer {inherit pkgs;};
   };
 }
