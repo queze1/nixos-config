e73cb817761c47a1a26e719e08589b6aa6b8bc32
queze1 <52340127+queze1@users.noreply.github.com>
2026-09-17 07:48:05 +1000

    fix: fix subsituting devdependency

--- outputs/packages/actions-languageserver.nix
+++ outputs/packages/actions-languageserver.nix
@@ -8,8 +8,33 @@
       rev = "5e28810649ba41b5483753ba74f976f83856a504";
       hash = "sha256-cf5Bww0aT8ftBcHaX6ST17DbXEqlUsMKhxkl6z4yy5c=";
     };
+    packageLock = let
+      lock = builtins.fromJSON (builtins.readFile "${src}/package-lock.json");
+    in
+      lock
+      // {
+        packages =
+          lock.packages
+          // {
+            "languageservice" =
+              lock.packages.languageservice
+              // {
+                devDependencies =
+                  lock.packages.languageservice.devDependencies
+                  // {
+                    "rest-api-description" = "file:${restApiDescription}";
+                  };
+              };
+            "node_modules/rest-api-description" =
+              lock.packages."node_modules/rest-api-description"
+              // {
+                resolved = "file:${restApiDescription}";
+              };
+          };
+      };
     npmDeps = pkgs.importNpmLock {
       npmRoot = src;
+      inherit packageLock;
       packageSourceOverrides."node_modules/rest-api-description" = restApiDescription;
     };
   in {
@@ -25,7 +50,9 @@
 
       prePatch = ''
         substituteInPlace languageservice/package.json \
-          --replace-fail 'github:github/rest-api-description' 'file:${restApiDescription}'
+          --replace-fail \
+            'github:github/rest-api-description' \
+            'file:${restApiDescription}'
       '';
     };
   };
