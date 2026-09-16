373e936d17287615f8ff5ca1926ce745fcf3cf30
queze1 <52340127+queze1@users.noreply.github.com>
2026-09-17 07:24:47 +1000

    fix: rewrite a dependency

--- outputs/packages/actions-languageserver.nix
+++ outputs/packages/actions-languageserver.nix
@@ -2,6 +2,15 @@
   perSystem = {pkgs, ...}: let
     src = inputs.actions-languageservices;
     version = (builtins.fromJSON (builtins.readFile "${src}/languageserver/package.json")).version;
+    restApiDescription = pkgs.fetchgit {
+      url = "https://github.com/github/rest-api-description.git";
+      rev = "5e28810649ba41b5483753ba74f976f83856a504";
+      hash = "sha256-cf5Bww0aT8ftBcHaX6ST17DbXEqlUsMKhxkl6z4yy5c=";
+    };
+    npmDeps = pkgs.importNpmLock {
+      npmRoot = src;
+      packageSourceOverrides."node_modules/rest-api-description" = restApiDescription;
+    };
   in {
     myPackages.actions-languageserver = pkgs.buildNpmPackage {
       pname = "actions-languageserver";
@@ -10,16 +19,13 @@
       nativeBuildInputs = [pkgs.git pkgs.openssh];
 
       npmWorkspace = "languageserver";
-      npmDeps = pkgs.importNpmLock {
-        npmRoot = src;
-        # fetcherOpts = {
-        #   # Rewrite SSH to HTTPS
-        #   "node_modules/rest-api-description" = {
-        #     url = "https://github.com/github/rest-api-description.git";
-        #   };
-        # };
-      };
+      inherit npmDeps;
       npmConfigHook = pkgs.importNpmLock.npmConfigHook;
+
+      prePatch = ''
+        substituteInPlace languageservice/package.json \
+          --replace-fail 'github:github/rest-api-description' 'file:${restApiDescription}'
+      '';
     };
   };
 }
