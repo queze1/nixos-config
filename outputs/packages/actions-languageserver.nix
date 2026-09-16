2822e99287da9abb8fb2f41480be408352847a6e
queze1 <52340127+queze1@users.noreply.github.com>
2026-09-17 07:39:27 +1000

    fix: test using npmdepshash

--- outputs/packages/actions-languageserver.nix
+++ outputs/packages/actions-languageserver.nix
@@ -2,31 +2,13 @@
   perSystem = {pkgs, ...}: let
     src = inputs.actions-languageservices;
     version = (builtins.fromJSON (builtins.readFile "${src}/languageserver/package.json")).version;
-    restApiDescription = pkgs.fetchFromGitHub {
-      owner = "github";
-      repo = "rest-api-description";
-      rev = "5e28810649ba41b5483753ba74f976f83856a504";
-      hash = "sha256-cf5Bww0aT8ftBcHaX6ST17DbXEqlUsMKhxkl6z4yy5c=";
-    };
-    npmDeps = pkgs.importNpmLock {
-      npmRoot = src;
-      packageSourceOverrides."node_modules/rest-api-description" = restApiDescription;
-    };
   in {
     myPackages.actions-languageserver = pkgs.buildNpmPackage {
       pname = "actions-languageserver";
       inherit src version;
-
       nativeBuildInputs = [pkgs.git pkgs.openssh];
-
       npmWorkspace = "languageserver";
-      inherit npmDeps;
-      npmConfigHook = pkgs.importNpmLock.npmConfigHook;
-
-      prePatch = ''
-        substituteInPlace languageservice/package.json \
-          --replace-fail 'github:github/rest-api-description' 'file:${restApiDescription}'
-      '';
+      npmDepsHash = "sha256-tuEfyePwlOy2/mOPdXbqJskO6IowvAP4DWg8xSZwbJw=";
     };
   };
 }
