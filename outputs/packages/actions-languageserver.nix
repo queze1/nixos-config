98f994e5f85b304fea42a71a0801a851eb7f36ae
queze1 <52340127+queze1@users.noreply.github.com>
2026-09-16 13:20:25 +1000

    style: format filebrowser quantum

--- outputs/packages/actions-languageserver.nix
+++ outputs/packages/actions-languageserver.nix
@@ -3,23 +3,16 @@
   lib,
   ...
 }: {
-  perSystem = {pkgs, ...}: {
+  perSystem = {pkgs, ...}: let
+    # Read from package.json
+    version = (builtins.fromJSON (builtins.readFile "${inputs.actions-languageservices}/languageserver/package.json")).version;
+  in {
     myPackages.actions-languageserver = pkgs.buildNpmPackage {
       pname = "actions-languageserver";
-      version = (builtins.fromJSON (builtins.readFile "${inputs.actions-languageservices}/languageserver/package.json")).version;
+      inherit version;
       src = inputs.actions-languageservices;
+      npmWorkspace = "languageserver";
       npmDepsHash = lib.fakeHash;
-      npmBuildScript = "build --workspace @actions/languageserver";
-      nativeBuildInputs = [pkgs.makeWrapper];
-      installPhase = ''
-        runHook preInstall
-        install -d $out/lib/actions-languageserver $out/bin
-        cp -r languageserver/dist $out/lib/actions-languageserver/
-        cp -r languageserver/bin $out/lib/actions-languageserver/
-        makeWrapper ${pkgs.nodejs}/bin/node $out/bin/actions-languageserver \
-          --add-flags "$out/lib/actions-languageserver/bin/actions-languageserver"
-        runHook postInstall
-      '';
     };
   };
 }
--- outputs/packages/filebrowser-quantum.nix
+++ outputs/packages/filebrowser-quantum.nix
@@ -1,48 +1,46 @@
 {inputs, ...}: {
-  perSystem = {pkgs, ...}: {
-    myPackages.filebrowser-quantum = pkgs.buildGo127Module (
-      let
-        version = "unstable-${inputs.filebrowser-quantum.lastModifiedDate}";
-        frontend = pkgs.buildNpmPackage {
-          pname = "filebrowser-quantum-frontend";
-          inherit version;
-          src = inputs.filebrowser-quantum;
-          sourceRoot = "source/frontend";
-          npmDepsHash = "sha256-pF524vtMXXmgmgncKD84yUVJhPVX/fqMQmdBACfsG/Q=";
-          postPatch = ''
-            chmod -R u+w ../backend
-          '';
-          installPhase = ''
-            install -d $out
-            cp -r ../backend/internal/web/embed/. $out
-          '';
-        };
-      in {
-        pname = "filebrowser-quantum";
-        inherit version;
-        src = inputs.filebrowser-quantum;
+  perSystem = {pkgs, ...}: let
+    version = "unstable-${inputs.filebrowser-quantum.lastModifiedDate}";
+    frontend = pkgs.buildNpmPackage {
+      pname = "filebrowser-quantum-frontend";
+      inherit version;
+      src = inputs.filebrowser-quantum;
+      sourceRoot = "source/frontend";
+      npmDepsHash = "sha256-pF524vtMXXmgmgncKD84yUVJhPVX/fqMQmdBACfsG/Q=";
+      postPatch = ''
+        chmod -R u+w ../backend
+      '';
+      installPhase = ''
+        install -d $out
+        cp -r ../backend/internal/web/embed/. $out
+      '';
+    };
+  in {
+    myPackages.filebrowser-quantum = pkgs.buildGo127Module {
+      pname = "filebrowser-quantum";
+      inherit version;
+      src = inputs.filebrowser-quantum;
 
-        sourceRoot = "source/backend";
-        vendorHash = "sha256-GfeKD/VtUfp2ld3irRkBxGWO2ixcuU0ODjtwgzGFOv8=";
-        subPackages = ["."];
-        doCheck = false;
-        nativeBuildInputs = [pkgs.makeWrapper];
+      sourceRoot = "source/backend";
+      vendorHash = "sha256-GfeKD/VtUfp2ld3irRkBxGWO2ixcuU0ODjtwgzGFOv8=";
+      subPackages = ["."];
+      doCheck = false;
+      nativeBuildInputs = [pkgs.makeWrapper];
 
-        preBuild = ''
-          cp -r ${frontend}/. internal/web/embed
-        '';
+      preBuild = ''
+        cp -r ${frontend}/. internal/web/embed
+      '';
 
-        postInstall = ''
-          mv $out/bin/backend $out/bin/filebrowser-quantum
-        '';
+      postInstall = ''
+        mv $out/bin/backend $out/bin/filebrowser-quantum
+      '';
 
-        postFixup = ''
-          wrapProgram $out/bin/filebrowser-quantum \
-            --prefix PATH : ${pkgs.lib.makeBinPath [pkgs.ffmpeg]}
-        '';
+      postFixup = ''
+        wrapProgram $out/bin/filebrowser-quantum \
+          --prefix PATH : ${pkgs.lib.makeBinPath [pkgs.ffmpeg]}
+      '';
 
-        meta.mainProgram = "filebrowser-quantum";
-      }
-    );
+      meta.mainProgram = "filebrowser-quantum";
+    };
   };
 }
