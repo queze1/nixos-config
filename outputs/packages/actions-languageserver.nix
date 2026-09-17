2295f678459bd0f46c3ac37baf512248e69fdbe8
queze1 <52340127+queze1@users.noreply.github.com>
2026-09-17 10:01:41 +1000

    fix: try to use fixed derivation

--- outputs/packages/actions-languageserver.nix
+++ outputs/packages/actions-languageserver.nix
@@ -1,47 +1,29 @@
 {inputs, ...}: {
-  perSystem = {pkgs, ...}: let
+  perSystem = {
+    lib,
+    pkgs,
+    ...
+  }: let
     src = inputs.actions-languageservices;
     version = (builtins.fromJSON (builtins.readFile "${src}/languageserver/package.json")).version;
-
-    patchedSrc =
-      pkgs.runCommand "actions-languageservices-patched" {
-        nativeBuildInputs = [
-          pkgs.nodejs
-          pkgs.git
-          pkgs.writableTmpDirAsHomeHook
-        ];
-
-        SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-        NIX_SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-
-        outputHashMode = "recursive";
-        outputHashAlgo = "sha256";
-        outputHash = "sha256-tApLjSS/5ZPXjQy+CgTR/hRkCVcrq+bhTA3Dv8NX2fE=";
-      } ''
-        cp -R ${src}/. $out
-        chmod -R u+w $out
-
-        # Regenerate the lock file
-        rm -f $out/package-lock.json
-        npm install --package-lock-only \
-                    --prefix $out \
-                    --ignore-scripts --no-audit --no-fund \
-                    --loglevel verbose
-      '';
   in {
-    myPackages.actions-languageservices-patched-src = patchedSrc;
-
     myPackages.actions-languageserver = pkgs.buildNpmPackage {
       pname = "actions-languageserver";
-      inherit version;
-      src = patchedSrc;
+      inherit version src;
 
-      nativeBuildInputs = [pkgs.git pkgs.openssh];
+      nativeBuildInputs = [
+        pkgs.git
+      ];
+      SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+      NIX_SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
 
       npmWorkspace = "languageserver";
-      npmDeps = pkgs.importNpmLock {npmRoot = patchedSrc;};
-      npmConfigHook = pkgs.importNpmLock.npmConfigHook;
+      npmDepsHash = lib.fakeHash;
       npmFlags = ["--loglevel" "verbose"];
+
+      outputHashMode = "recursive";
+      outputHashAlgo = "sha256";
+      outputHash = lib.fakeHash;
     };
   };
 }
