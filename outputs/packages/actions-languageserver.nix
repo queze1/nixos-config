9cc0d30c7f3151dda7879f4704e1f5595ded1589
queze1 <52340127+queze1@users.noreply.github.com>
2026-09-17 10:38:54 +1000

    fix: use patched src with buildnpmmodule

--- outputs/packages/actions-languageserver.nix
+++ outputs/packages/actions-languageserver.nix
@@ -6,17 +6,46 @@
   }: let
     src = inputs.actions-languageservices;
     version = (builtins.fromJSON (builtins.readFile "${src}/languageserver/package.json")).version;
+
+    patchedSrc =
+      pkgs.runCommand "actions-languageservices-patched" {
+        nativeBuildInputs = [
+          pkgs.nodejs
+          pkgs.git
+          pkgs.writableTmpDirAsHomeHook
+        ];
+
+        SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+        NIX_SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+
+        outputHashMode = "recursive";
+        outputHashAlgo = "sha256";
+        outputHash = "sha256-tApLjSS/5ZPXjQy+CgTR/hRkCVcrq+bhTA3Dv8NX2fE=";
+      } ''
+        cp -R ${src}/. $out
+        chmod -R u+w $out
+
+        # Regenerate the lock file
+        rm -f $out/package-lock.json
+        npm install --package-lock-only \
+                    --prefix $out \
+                    --ignore-scripts --no-audit --no-fund \
+                    --loglevel verbose
+      '';
   in {
+    myPackages.actions-languageservices-patched-src = patchedSrc;
+
     myPackages.actions-languageserver = pkgs.buildNpmPackage {
       pname = "actions-languageserver";
-      inherit version src;
+      src = patchedSrc;
+      inherit version;
 
       SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
       NIX_SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
 
       npmWorkspace = "languageserver";
       npmDepsFetcherVersion = 2;
-      npmDepsHash = "sha256-7ZsnU7aGT/OCEmuy2ndSmN27mwpOUKd1zG07oQjhc7c=";
+      npmDepsHash = lib.fakeHash;
       npmFlags = ["--loglevel" "verbose"];
 
       # postPatch = ''
