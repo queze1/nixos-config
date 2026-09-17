d5f1edaf3ba18d86e35d6984882620a757d8c5a9
queze1 <52340127+queze1@users.noreply.github.com>
2026-09-17 10:48:48 +1000

    Revert "fix: try postpatch"
    This reverts commit 4fd9aa57429c2cd465701c84ea7b744292e1ba78.

--- outputs/packages/actions-languageserver.nix
+++ outputs/packages/actions-languageserver.nix
@@ -48,13 +48,13 @@
       npmDepsHash = lib.fakeHash;
       npmFlags = ["--loglevel" "verbose"];
 
-      postPatch = ''
-        find . -name package-lock.json -type f -exec sed -i \
-          -e 's#git+ssh://git@github.com/#https://github.com/#g' \
-          -e 's#ssh://git@github.com/#https://github.com/#g' \
-          -e 's#git@github.com:#https://github.com/#g' \
-          {} +
-      '';
+      # postPatch = ''
+      #   find . -name package-lock.json -type f -exec sed -i \
+      #     -e 's#git+ssh://git@github.com/#https://github.com/#g' \
+      #     -e 's#ssh://git@github.com/#https://github.com/#g' \
+      #     -e 's#git@github.com:#https://github.com/#g' \
+      #     {} +
+      # '';
     };
   };
 }
