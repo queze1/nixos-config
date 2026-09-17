fbc7ea022b4d53b4c8fecddb6067a0e06a578cab
queze1 <52340127+queze1@users.noreply.github.com>
2026-09-17 10:09:03 +1000

    fix: use npmdepsfetcherversion 2

--- outputs/packages/actions-languageserver.nix
+++ outputs/packages/actions-languageserver.nix
@@ -15,16 +15,17 @@
       NIX_SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
 
       npmWorkspace = "languageserver";
+      npmDepsFetcherVersion = 2;
       npmDepsHash = lib.fakeHash;
       npmFlags = ["--loglevel" "verbose"];
 
-      postPatch = ''
-        find . -name package-lock.json -type f -exec sed -i \
-          -e 's#git+ssh://git@github.com/#https://github.com/#g' \
-          -e 's#ssh://git@github.com/#https://github.com/#g' \
-          -e 's#git@github.com:#https://github.com/#g' \
-          {} +
-      '';
+      # postPatch = ''
+      #   find . -name package-lock.json -type f -exec sed -i \
+      #     -e 's#git+ssh://git@github.com/#https://github.com/#g' \
+      #     -e 's#ssh://git@github.com/#https://github.com/#g' \
+      #     -e 's#git@github.com:#https://github.com/#g' \
+      #     {} +
+      # '';
 
       outputHashMode = "recursive";
       outputHashAlgo = "sha256";
